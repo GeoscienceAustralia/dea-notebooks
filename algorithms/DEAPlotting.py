e7393d3d789ecc7e737bf3b9eec3fd3379e71f92
Claire <claire.krause@ga.gov.au>
2018-03-07 12:28:20 +1100

    Updated algorithms

--- algorithms/DEAPlotting.py
+++ algorithms/DEAPlotting.py
@@ -1,12 +1,30 @@
+## DEAPlotting.py
+'''
+This file contains a set of python functions for plotting DEA data.
+Available functions:
+thre_band_image
+three_band_image_subplots
+write_to_geotiff
+
+Last modified: March 2018
+Author: Claire Krause
+
+'''
+
 import numpy as np
 from skimage import exposure
 import matplotlib.pyplot as plt
 import rasterio
 
-def threeBandImage(ds, bands, time = 0, figsize = [10,10], projection = 'projected'):
+def three_band_image(ds, bands, time = 0, figsize = [10,10], projection = 'projected'):
     '''
-    threeBandImage takes three spectral bands and plots them on the RGB bands of an image. 
+    threeBandImage takes three spectral bands and plots them on the RGB bands of an 
+    image. 
     
+    Last modified: March 2018
+    Author: Mike Barnes
+    Modified by: Claire Krause
+
     Inputs: 
     ds -   Dataset containing the bands to be plotted
     bands - list of three bands to be plotted
@@ -14,7 +32,8 @@ def threeBandImage(ds, bands, time = 0, figsize = [10,10], projection = 'project
     Optional:
     time - Index value of the time dimension of ds to be plotted
     figsize - dimensions for the output figure
-    projection - options are 'projected' or 'geographic'. To determine if the image is in degrees or northings
+    projection - options are 'projected' or 'geographic'. To determine if the image is 
+    in degrees or northings
     '''
     t, y, x = ds[bands[0]].shape
     rawimg = np.zeros((y,x,3), dtype = np.float32)
@@ -35,11 +54,17 @@ def threeBandImage(ds, bands, time = 0, figsize = [10,10], projection = 'project
         ax.set_xlabel('Eastings', fontweight = 'bold')
         ax.set_ylabel('Northings', fontweight = 'bold')
 
-def threeBandImage_subplots(ds, bands, num_cols, figsize = [10,10], projection = 'projected', left  = 0.125, 
-                            right = 0.9, bottom = 0.1, top = 0.9, wspace = 0.2, hspace = 0.4):
+def three_band_image_subplots(ds, bands, num_cols, figsize = [10,10], 
+                              projection = 'projected', left  = 0.125, 
+                              right = 0.9, bottom = 0.1, top = 0.9, 
+                              wspace = 0.2, hspace = 0.4):
     '''
-    threeBandImage_subplots takes three spectral bands and multiple time steps, and plots them 
-    on the RGB bands of an image. 
+    threeBandImage_subplots takes three spectral bands and multiple time steps, 
+    and plots them on the RGB bands of an image. 
+
+    Last modified: March 2018
+    Author: Mike Barnes
+    Modified by: Claire Krause
     
     Inputs: 
     ds -   Dataset containing the bands to be plotted
@@ -48,19 +73,21 @@ def threeBandImage_subplots(ds, bands, num_cols, figsize = [10,10], projection =
     
     Optional:
     figsize - dimensions for the output figure
-    projection - options are 'projected' or 'geographic'. To determine if the image is in degrees or northings
-    left  = 0.125  # the space on the left side of the subplots of the figure
-    right = 0.9    # the space on the right side of the subplots of the figure
-    bottom = 0.1   # the space on the bottom of the subplots of the figure
-    top = 0.9      # the space on the top of the subplots of the figure
-    wspace = 0.2   # the amount of width reserved for blank space between subplots
-    hspace = 0.2   # the amount of height reserved for white space between subplots
+    projection - options are 'projected' or 'geographic'. To determine if the image 
+                 is in degrees or northings
+    left  - the space on the left side of the subplots of the figure
+    right - the space on the right side of the subplots of the figure
+    bottom - the space on the bottom of the subplots of the figure
+    top - the space on the top of the subplots of the figure
+    wspace - the amount of width reserved for blank space between subplots
+    hspace - the amount of height reserved for white space between subplots
     '''
     # Find the number of rows/columns we need, based on the number of time steps in ds
     timesteps = ds.time.size
     num_rows = int(ceil(timesteps/num_cols))
     fig, axes = plt.subplots(num_rows, num_cols, figsize = figsize)
-    fig.subplots_adjust(left  = left, right = right, bottom = bottom, top = top, wspace = wspace, hspace = hspace)
+    fig.subplots_adjust(left  = left, right = right, bottom = bottom, top = top, 
+                        wspace = wspace, hspace = hspace)
     numbers = 0
     try:
         for ax in axes.flat:
@@ -82,25 +109,40 @@ def threeBandImage_subplots(ds, bands, num_cols, figsize = [10,10], projection =
                 ax.set_ylabel('Northings', fontweight = 'bold', fontsize = 10)
             numbers = numbers + 1
     except IndexError:
-        # This error will pop up if there are not enough scenes to fill the number of rows x columns, so we can 
-        # safely ignore it
+        # This error will pop up if there are not enough scenes to fill the number of 
+        # rows x columns, so we can safely ignore it
         fig.delaxes(ax)
         plt.draw()    
 
 def write_to_geotiff(filename, data):
-    '''this function uses rasterio and numpy to write a multi-band geotiff for one timeslice, or for
-    a single composite image. It assumes the input data is an xarray dataset (note, dataset not dataarray)
-    and that you have crs and affine objects attached, and that you are using float data. future users
-    may wish to assert that these assumptions are correct. Bex Dunn+ Josh Sixsmith 260218'''
+    '''
+    this function uses rasterio and numpy to write a multi-band geotiff for one 
+    timeslice, or for a single composite image. It assumes the input data is an 
+    xarray dataset (note, dataset not dataarray) and that you have crs and affine 
+    objects attached, and that you are using float data. future users
+    may wish to assert that these assumptions are correct.
+
+    inputs
+    filename - string containing filename to write out to
+    data - dataset to write out
+
+    Note: this function cuurrently requires the data have lat/lon only, i.e. no
+    time dimension
+
+    Last modified: March 2018
+    Authors: Bex Dunn and Josh Sixsmith
+    Modified by: Claire Krause
+    '''
     
     kwargs = {'driver': 'GTiff', 
-              'count': len(data.data_vars),#geomedian no time dim
+              'count': len(data.data_vars), #geomedian no time dim
               'width': data.sizes['x'], 'height': data.sizes['y'],
               'crs' : data.crs.crs_str,
               'transform':data.affine,
               'dtype': data.green.values.dtype,
               'nodata': 0,
-              'compress': 'deflate', 'zlevel': 4, 'predictor': 3 }#for ints use 2 for floats use 3}
+              'compress': 'deflate', 'zlevel': 4, 'predictor': 3 }
+              #for ints use 2 for floats use 3}
 
     with rasterio.open(filename, 'w', **kwargs) as src:
         for i, band in enumerate(data.data_vars):
