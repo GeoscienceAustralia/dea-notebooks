8cf57f2e27504180ea783995dac513effc2ab51e
Robbi Bishop-Taylor <robbibt@gmail.com>
2018-04-16 10:48:15 +1000

    Improved documentation of array_to_geotiff (#49)

--- algorithms/DEADataHandling.py
+++ algorithms/DEADataHandling.py
@@ -283,17 +283,31 @@ def dataset_to_geotiff(filename, data):
 def array_to_geotiff(fname, data, geo_transform, projection,
                      nodata_val=0, dtype=gdal.GDT_Float32):
     """
-    Create a single band GeoTIFF file with data from array. Because this
-    works with simple arrays rather than xarray datasets from DEA, it requires
-    the use to pass in geotransform and projection data for the output raster
+    Create a single band GeoTIFF file with data from an array. 
+    
+    Because this works with simple arrays rather than xarray datasets from DEA, it requires
+    geotransform info ("(upleft_x, x_size, x_rotation, upleft_y, y_rotation, y_size)") and 
+    projection data (in "WKT" format) for the output raster. These are typically obtained from 
+    an existing raster using the following GDAL calls:
+    
+    # import gdal
+    # gdal_dataset = gdal.Open(raster_path)
+    # geotrans = gdal_dataset.GetGeoTransform()
+    # prj = gdal_dataset.GetProjection()
+    
+    ...or alternatively, directly from an xarray dataset:
+    
+    # geotrans = xarraydataset.geobox.transform.to_gdal()
+    # prj = xarraydataset.geobox.crs.wkt
 
     Last modified: March 2018
     Author: Robbi Bishop-Taylor
 
     :attr fname: output file path
     :attr data: input array
-    :attr geo_transform: geotransform for output raster
-    :attr projection: projection for output raster
+    :attr geo_transform: geotransform for output raster; 
+    			 e.g. "(upleft_x, x_size, x_rotation, upleft_y, y_rotation, y_size)"
+    :attr projection: projection for output raster (in "WKT" format)
     :attr nodata_val: value to convert to nodata in output raster; default 0
     :attr dtype: value to convert to nodata in output raster; default gdal.GDT_Float32
     """
