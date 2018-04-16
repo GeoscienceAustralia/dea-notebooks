48625ad706672efb17f4b1a66f3b50891eda2c90
Robbi Bishop-Taylor <robbibt@gmail.com>
2018-04-16 14:19:13 +1000

    Rename to SpatialTools.py and add new functions (#54)
    * Rename to SpatialTools.py and add new functions
    
      New functions: indices_to_coords, coords_to_indices, raster_randomsample. Function moved from DEADataHandling: array_to_geotiff
    
    * Remove array_to_geotiff to SpatialTools.py

--- algorithms/DEADataHandling.py
+++ algorithms/DEADataHandling.py
@@ -1,15 +1,15 @@
 ## DEADataHandling.py
 '''
-This file contains a set of python functions for handling data within DEA.
+This file contains a set of python functions for handling data within DEA. If a function does not use 
+DEA functionality (for example, dc.load or xarrays), it may be better suited for inclusion in SpatialTools.py.
 Available functions:
 
     load_nbarx
     load_sentinel
     tasseled_cap
     dataset_to_geotiff
-    array_to_geotiff
 
-Last modified: March 2018
+Last modified: April 2018
 Author: Claire Krause
 Modified by: Robbi Bishop-Taylor
 
@@ -278,54 +278,3 @@ def dataset_to_geotiff(filename, data):
     with rasterio.open(filename, 'w', **kwargs) as src:
         for i, band in enumerate(data.data_vars):
             src.write(data[band].data, i + 1)
-
-
-def array_to_geotiff(fname, data, geo_transform, projection,
-                     nodata_val=0, dtype=gdal.GDT_Float32):
-    """
-    Create a single band GeoTIFF file with data from an array. 
-    
-    Because this works with simple arrays rather than xarray datasets from DEA, it requires
-    geotransform info ("(upleft_x, x_size, x_rotation, upleft_y, y_rotation, y_size)") and 
-    projection data (in "WKT" format) for the output raster. These are typically obtained from 
-    an existing raster using the following GDAL calls:
-    
-    # import gdal
-    # gdal_dataset = gdal.Open(raster_path)
-    # geotrans = gdal_dataset.GetGeoTransform()
-    # prj = gdal_dataset.GetProjection()
-    
-    ...or alternatively, directly from an xarray dataset:
-    
-    # geotrans = xarraydataset.geobox.transform.to_gdal()
-    # prj = xarraydataset.geobox.crs.wkt
-
-    Last modified: March 2018
-    Author: Robbi Bishop-Taylor
-
-    :attr fname: output file path
-    :attr data: input array
-    :attr geo_transform: geotransform for output raster; 
-    			 e.g. "(upleft_x, x_size, x_rotation, upleft_y, y_rotation, y_size)"
-    :attr projection: projection for output raster (in "WKT" format)
-    :attr nodata_val: value to convert to nodata in output raster; default 0
-    :attr dtype: value to convert to nodata in output raster; default gdal.GDT_Float32
-    """
-
-    # Set up driver
-    driver = gdal.GetDriverByName('GTiff')
-
-    # Create raster of given size and projection
-    rows, cols = data.shape
-    dataset = driver.Create(fname, cols, rows, 1, dtype)
-    dataset.SetGeoTransform(geo_transform)
-    dataset.SetProjection(projection)
-
-    # Write data to array and set nodata values
-    band = dataset.GetRasterBand(1)
-    band.WriteArray(data)
-    band.SetNoDataValue(nodata_val)
-
-    # Close file
-    dataset = None
-
