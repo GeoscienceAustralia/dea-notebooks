805d29de78bdee7cc0c23f7bc7c9a7945cbe4c43
Claire <claire.krause@ga.gov.au>
2018-05-04 15:07:23 +1000

    Initial commit

--- algorithms/DEADataHandling.py
+++ algorithms/DEADataHandling.py
@@ -266,16 +266,28 @@ def dataset_to_geotiff(filename, data):
     Note: this function cuurrently requires the data have lat/lon only, i.e. no
     time dimension
     '''
-
-    kwargs = {'driver': 'GTiff',
-              'count': len(data.data_vars),  # geomedian no time dim
-              'width': data.sizes['x'], 'height': data.sizes['y'],
-              'crs': data.crs.crs_str,
-              'transform': data.affine,
-              'dtype': list(data.data_vars.values())[0].values.dtype,
-              'nodata': 0,
-              'compress': 'deflate', 'zlevel': 4, 'predictor': 3}
-    # for ints use 2 for floats use 3}
+    
+    try:
+        test = data.crs.crs_str
+        kwargs = {'driver': 'GTiff',
+                  'count': len(data.data_vars),  # geomedian no time dim
+                  'width': data.sizes['x'], 'height': data.sizes['y'],
+                  'crs': data.crs.crs_str,
+                  'affine': data.affine,
+                  'dtype': list(data.data_vars.values())[0].values.dtype,
+                  'nodata': 0,
+                  'compress': 'deflate', 'zlevel': 4, 'predictor': 3}
+        # for ints use 2 for floats use 3}
+    except:
+        kwargs = {'driver': 'GTiff',
+                  'count': len(data.data_vars),  # geomedian no time dim
+                  'width': data.sizes['x'], 'height': data.sizes['y'],
+                  'crs': data.crs,
+                  'affine': data.affine,
+                  'dtype': list(data.data_vars.values())[0].values.dtype,
+                  'nodata': 0,
+                  'compress': 'deflate', 'zlevel': 4, 'predictor': 3}
+        # for ints use 2 for floats use 3}
 
     with rasterio.open(filename, 'w', **kwargs) as src:
         for i, band in enumerate(data.data_vars):
