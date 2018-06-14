9de8021851d3bf82a1324a4c474ffd63690362c0
Bex Dunn <BexDunn@users.noreply.github.com>
2018-06-14 11:50:15 +1000

    Changing back from auto_combine to concat (#140)
    auto_combine doesn't have an option to preserve attributes, so we lose our crs objects

--- Scripts/DEADataHandling.py
+++ Scripts/DEADataHandling.py
@@ -341,7 +341,7 @@ def load_clearlandsat(dc, query, sensors=['ls5', 'ls7', 'ls8'], bands_of_interes
 
     # Concatenate all sensors into one big xarray dataset, and then sort by time
     print('Combining and sorting ls5, ls7 and ls8 data')
-    combined_ds = xr.auto_combine(filtered_sensors)
+    combined_ds = xr.concat(filtered_sensors, dim='time')
     combined_ds = combined_ds.sortby('time')
                                                                
     #Filter to replace no data values with nans
