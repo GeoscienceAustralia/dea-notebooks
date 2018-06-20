96ffea8a656105516820c5e3c83e6024d882115a
Erin Telfer <erin.telfer@ga.gov.au>
2018-06-21 09:02:01 +1000

    updated 21/06/2018

--- scripts/Sc02_unstack_netcdf.sh
+++ scripts/Sc02_unstack_netcdf.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for ncfile in /g/data/u46/users/ext547/ewater/input_data/Temperature/degrees_celcius_and_clipped/celcius/*.nc; do
+for ncfile in /g/data/u46/users/ext547/ewater/input_data/Rainfall/daily_sum/*.nc; do
 	echo $ncfile
 	cdo -splitsel,1 $ncfile split_
 	for f in split_*.nc; do
 		mytime=$(cdo showtimestamp $f 2> /dev/null | tr -d '[:space:]')
-		mv $f Tair_$mytime.nc
+		mv $f daily_sum_rainfall_$mytime.nc
 	done
+
 done
 
 
