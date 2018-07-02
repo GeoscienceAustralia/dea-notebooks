1d2911d82ef1fbd0f34d23439bb114bb7fec756a
Erin Telfer <erin.telfer@ga.gov.au>
2018-07-03 09:42:39 +1000

    updated 06/07/2018

--- scripts/Sc08_replace_fill_value_temp.sh
+++ scripts/Sc08_replace_fill_value_temp.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 
-for ncfile in /g/data/u46/users/ext547/ewater/input_data/Temperature/degrees_celcius_and_clipped/celcius/original/*.nc; do
+for ncfile in /g/data/u46/users/ext547/ewater/input_data/temperature/temperature_nc/daily_mean/*.nc; do
     echo `basename $ncfile`
     ncatted -a _FillValue,Tair,o,s,-9999 $ncfile
 
