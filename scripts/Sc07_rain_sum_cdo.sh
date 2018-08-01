aa543c547d1dff5e9c2bbc1734ef87eea86f3246
Erin Telfer <erin.telfer@ga.gov.au>
2018-08-01 14:06:31 +1000

    updated 01/08/2018

--- scripts/Sc07_rain_sum_cdo.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-for ncfile in /g/data/u46/users/ext547/ewater/input_data/Rainfall/3B42_raw_3hourly_all_data_hqp.nc; do
-    echo -n .
-    cdo daysum $ncfile daily_sum_`basename $ncfile`
-done
