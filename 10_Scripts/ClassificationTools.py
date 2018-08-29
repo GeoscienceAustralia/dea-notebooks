6ce48c87487b10d8f6a0036572c37e0c06d50056
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2018-08-29 12:14:01 +1000

    Add all revised files for staging (#193)

--- 10_Scripts/ClassificationTools.py
+++ 10_Scripts/ClassificationTools.py
@@ -86,14 +86,14 @@ def randomforest_train(train_shps, train_field, data_func, data_func_params={},
         '''Function for rasterising a Geopandas datadframe using Rasterio'''
 
         with rasterio.open(out_rast, 'w', **meta) as out:
-            out_arr = out.read(1)
+        
+            out_arr = np.zeros(shape=(meta['height'], meta['height']))
 
             # this is where we create a generator of geom, value pairs to use in rasterizing
             shapes = ((geom,value) for geom, value in zip(convert_to_rast.geometry, 
                                                           convert_to_rast[field]))
 
             burned = features.rasterize(shapes=shapes, fill=0, out=out_arr, transform=out.transform)
-            out.write_band(1, burned)    
 
         return burned 
     
@@ -159,7 +159,6 @@ def randomforest_train(train_shps, train_field, data_func, data_func_params={},
                                         out_rast='temp_raster.tif', 
                                         meta=meta, 
                                         field=train_field)
-            os.remove('temp_raster.tif')
 
             # Extract matching image sample data for each labelled pixel location
             is_train = np.nonzero(training_pixels)
