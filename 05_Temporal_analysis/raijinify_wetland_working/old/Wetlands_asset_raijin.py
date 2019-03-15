ee3917e7dd7c4d1c3719744f35bba3f22fec9946
BexDunn <bex.dunn@ga.gov.au>
2019-03-15 16:21:12 +1100

    backup stuff

--- 05_Temporal_analysis/raijinify_wetland_working/old/Wetlands_asset_raijin.py
+++ 05_Temporal_analysis/raijinify_wetland_working/old/Wetlands_asset_raijin.py
@@ -81,14 +81,10 @@ dc = datacube.Datacube(app='asset drill')
 poly_path='/g/data/r78/rjd547/Ramsar_Wetlands/ExplodedRAMSAR.shp'
 print(f'Shape file is {poly_path}')
 
-part = sys.argv[1] #take an argument from the command line (our parallelish scripte)
+#3333333part = sys.argv[1] #take an argument from the command line (our parallelish scripte)
+part = 2
 part = int(part)
 print(f'system argument received is {part}')
-
-#Desired number of chunks
-if len(sys.argv)<3:
-    print('Usage: this script takes 2 arguments - update your submission script')
-DesiredChunks = int(sys.argv[2])
 
 global Output_dir
 Output_dir = '/g/data/r78/rjd547/Ramsar_Wetlands/Ramsar_Outputs_1/'
@@ -97,11 +93,6 @@ Output_dir = '/g/data/r78/rjd547/Ramsar_Wetlands/Ramsar_Outputs_1/'
 time.sleep(5*part)
 #open the polygon
 
-
-#defaults 
-# part =2
-# DesiredChunks=64
-
 #this code tells us which polygon ids will be running on this particular (node?). Shapessubset will be the subset of polygons that our function
 #will run over. 
 with fiona.open(poly_path) as allshapes:
@@ -109,12 +100,17 @@ with fiona.open(poly_path) as allshapes:
         crs = geometry.CRS(allshapes.crs_wkt)
         #get the list of all the shapes in the shapefile
         ShapesList = list(allshapes)
+        #Desired number of chunks
+        #Set this to 64 because we want megamem to use 4x8x2 cpus (64)
+        DesiredChunks = 64
         ChunkSize = ceil(len(ShapesList)/DesiredChunks) #this was set due to Claire having 64000 polygons in her code
         print(f'chunk size is {ChunkSize}')
         print(f'There are {int(len(ShapesList)/ChunkSize)} generated chunks')
         shapessubset = allshapes[(part - 1) * ChunkSize: part * ChunkSize]
-        print(f'Running for polygon IDs in the range {(part - 1) * ChunkSize} to {part * ChunkSize}') 
-        
+        print(f'Running for polygon IDs in the range {(part - 1) * ChunkSize} to {part * ChunkSize}')  
+
+### define functions that are run in the mainline here
+
 def get_RAMSAR_polyName(shapefile):
     ''' function designed specifically for the RAMSAR wetlands australia shapefile. Takes the shapefile and extracts
     the ramsar name, wetland name and objectID from the ESRI shapefile format and turns it into a useful string for our output.
@@ -154,128 +150,128 @@ def get_masked_ls578_data(query, geom):
 def BigFunkyFunction(lilshape,crs):
     '''This is a function that does lots of things. It takes a single polygon and does all the things #FIXME '''
     ### This is set up to be shapefile-specific. I'm not sure this can be avoided, as often shapefiles are pretty specific..
+    first_geometry = lilshape['geometry']
+    polyName = get_RAMSAR_polyName(lilshape)
+    geom = geometry.Geometry(first_geometry, crs=crs)
+    query = {'geopolygon': geom}# this should run for all time, if there is no time set?
+    
+    #load and mask data. selecting data with more than 90% clear for the geobox around the polygon... #FIXME
+    ls578_ds, mask_xr= get_masked_ls578_data(query,geom)
+    
+    print('run tasselled cap transform')
+    #transform the nbart into tci
+    tci = TasseledCapTools.thresholded_tasseled_cap(ls578_ds,wetness_threshold=-350, drop=True , drop_tc_bands=True)
+
+    ### create a masked version of the extent of overthreshold wetness
+
+    #select only finite values (over threshold values)
+    tcw = xr.ufuncs.isfinite(tci.wetness_thresholded)
+
+    # #reapply the polygon mask
+    tcw = tcw.where(mask_xr==False)
+
+    ### load wofls and select only wet pixels
+    print('load wofls')
+    #load wofs
+    wofls = dc.load(product = 'wofs_albers', like=ls578_ds)
+
+    #only get wet obs
+    wetwofl = masking.make_mask(wofls, wet=True)
+
+    #match the wofs observations to the nbart
+    wetwofl=wetwofl.where(wofls.time==ls578_ds.time)
+
+    ### mask the wofs obs
+
+    #mask the wofs obs with the polygon mask
+    wetwofl = wetwofl.where(mask_xr==False)
+
+    ### load in fractional cover data
+    print('load fractional cover data')
+    #load the data according to our query
+    #choose a mask proportion to look for a clear timestep
+    fc_ds = DEADataHandling.load_clearlandsat(dc, query,product='fc',masked_prop=0.90)
+
+    ### mask FC with polygon
+
+    fc_ds = fc_ds.where(mask_xr==False)
+
+    ### mask FC with wetness
+
+    fc_ds_noTCW=fc_ds.where(tcw==False)
+
+    ### Calculate number of pixels in area of interest
+
+    #number of pixels in area of interest
+    pixels = (mask_xr==0).sum(dim=['x','y'])
+
+    mask_xr==0
+    mask_xr.count(dim=['x','y'])
+
+    #count number of wofs pixels
+    wofs_pixels = wetwofl.water.sum(dim=['x','y'])
+
+    #count percentage of area of wofs
+    wofs_area_percent = (wofs_pixels/pixels)*100
+
+    #count number of tcw pixels
+    tcw_pixel_count = tcw.sum(dim=['x','y'])
+
+    #calculate percentage area wet
+    tcw_area_percent = (tcw_pixel_count/pixels)*100
+
+    #calculate wet not wofs
+    tcw_less_wofs = tcw_area_percent-wofs_area_percent
+
+    ### Calculate the dominant fraction for each pixel in Fractional Cover
+
+    #drop data percentage and Unmixing Error
+    fc_tester = fc_ds_noTCW.drop(['data_perc','UE'])
+
+    #following robbi's advice, cast the dataset to a dataarray
+    maxFC = fc_tester.to_array(dim='variable', name='maxFC')
+
+    #turn FC array into integer only as nanargmax doesn't seem to handle floats the way we want it to
+    FC_int = maxFC.astype('int8')
+
+    #use numpy.nanargmax to get the index of the maximum value along the variable dimension
+    #BSPVNPV=np.nanargmax(FC_int, axis=0)
+    BSPVNPV=FC_int.argmax(dim='variable')
+
+    FC_mask=xr.ufuncs.isfinite(maxFC).all(dim='variable')
+
+    # #re-mask with nans to remove no-data
+    BSPVNPV=BSPVNPV.where(FC_mask)
+
+
+    FC_dominant = xr.Dataset({
+        'BS': (BSPVNPV==0).where(FC_mask),
+        'PV': (BSPVNPV==1).where(FC_mask),
+        'NPV': (BSPVNPV==2).where(FC_mask),
+    })
+
+    FC_count = FC_dominant.sum(dim=['x','y'])
+
+    #Fractional cover pixel count method
+    #Get number of FC pixels, divide by total number of pixels per polygon
+
+    Bare_soil_percent=(FC_count.BS/pixels)*100
+
+    Photosynthetic_veg_percent=(FC_count.PV/pixels)*100
+
+    NonPhotosynthetic_veg_percent=(FC_count.NPV/pixels)*100
+
+    NoData = 100 - wofs_area_percent- tcw_less_wofs - Photosynthetic_veg_percent - NonPhotosynthetic_veg_percent - Bare_soil_percent
+
+    #set up color palette
+    pal = [sns.xkcd_rgb["grey"],
+           sns.xkcd_rgb["cobalt blue"],
+           sns.xkcd_rgb["neon blue"],
+           sns.xkcd_rgb["grass"],
+           sns.xkcd_rgb["beige"],
+           sns.xkcd_rgb["brown"]]       
+    #try and figure out what the error is on one of the plots
     try:
-        first_geometry = lilshape['geometry']
-        polyName = get_RAMSAR_polyName(lilshape)
-        geom = geometry.Geometry(first_geometry, crs=crs)
-        query = {'geopolygon': geom}# this should run for all time, if there is no time set?
-
-        #load and mask data. selecting data with more than 90% clear for the geobox around the polygon... #FIXME
-        ls578_ds, mask_xr= get_masked_ls578_data(query,geom)
-
-        print('run tasselled cap transform')
-        #transform the nbart into tci
-        tci = TasseledCapTools.thresholded_tasseled_cap(ls578_ds,wetness_threshold=-350, drop=True , drop_tc_bands=True)
-
-        ### create a masked version of the extent of overthreshold wetness
-
-        #select only finite values (over threshold values)
-        tcw = xr.ufuncs.isfinite(tci.wetness_thresholded)
-
-        # #reapply the polygon mask
-        tcw = tcw.where(mask_xr==False)
-
-        ### load wofls and select only wet pixels
-        print('load wofls')
-        #load wofs
-        wofls = dc.load(product = 'wofs_albers', like=ls578_ds)
-
-        #only get wet obs
-        wetwofl = masking.make_mask(wofls, wet=True)
-
-        #match the wofs observations to the nbart
-        wetwofl=wetwofl.where(wofls.time==ls578_ds.time)
-
-        ### mask the wofs obs
-
-        #mask the wofs obs with the polygon mask
-        wetwofl = wetwofl.where(mask_xr==False)
-
-        ### load in fractional cover data
-        print('load fractional cover data')
-        #load the data according to our query
-        #choose a mask proportion to look for a clear timestep
-        fc_ds = DEADataHandling.load_clearlandsat(dc, query,product='fc',masked_prop=0.90)
-
-        ### mask FC with polygon
-
-        fc_ds = fc_ds.where(mask_xr==False)
-
-        ### mask FC with wetness
-
-        fc_ds_noTCW=fc_ds.where(tcw==False)
-
-        ### Calculate number of pixels in area of interest
-
-        #number of pixels in area of interest
-        pixels = (mask_xr==0).sum(dim=['x','y'])
-
-        mask_xr==0
-        mask_xr.count(dim=['x','y'])
-
-        #count number of wofs pixels
-        wofs_pixels = wetwofl.water.sum(dim=['x','y'])
-
-        #count percentage of area of wofs
-        wofs_area_percent = (wofs_pixels/pixels)*100
-
-        #count number of tcw pixels
-        tcw_pixel_count = tcw.sum(dim=['x','y'])
-
-        #calculate percentage area wet
-        tcw_area_percent = (tcw_pixel_count/pixels)*100
-
-        #calculate wet not wofs
-        tcw_less_wofs = tcw_area_percent-wofs_area_percent
-
-        ### Calculate the dominant fraction for each pixel in Fractional Cover
-
-        #drop data percentage and Unmixing Error
-        fc_tester = fc_ds_noTCW.drop(['data_perc','UE'])
-
-        #following robbi's advice, cast the dataset to a dataarray
-        maxFC = fc_tester.to_array(dim='variable', name='maxFC')
-
-        #turn FC array into integer only as nanargmax doesn't seem to handle floats the way we want it to
-        FC_int = maxFC.astype('int8')
-
-        #use numpy.nanargmax to get the index of the maximum value along the variable dimension
-        #BSPVNPV=np.nanargmax(FC_int, axis=0)
-        BSPVNPV=FC_int.argmax(dim='variable')
-
-        FC_mask=xr.ufuncs.isfinite(maxFC).all(dim='variable')
-
-        # #re-mask with nans to remove no-data
-        BSPVNPV=BSPVNPV.where(FC_mask)
-
-
-        FC_dominant = xr.Dataset({
-            'BS': (BSPVNPV==0).where(FC_mask),
-            'PV': (BSPVNPV==1).where(FC_mask),
-            'NPV': (BSPVNPV==2).where(FC_mask),
-        })
-
-        FC_count = FC_dominant.sum(dim=['x','y'])
-
-        #Fractional cover pixel count method
-        #Get number of FC pixels, divide by total number of pixels per polygon
-
-        Bare_soil_percent=(FC_count.BS/pixels)*100
-
-        Photosynthetic_veg_percent=(FC_count.PV/pixels)*100
-
-        NonPhotosynthetic_veg_percent=(FC_count.NPV/pixels)*100
-
-        NoData = 100 - wofs_area_percent- tcw_less_wofs - Photosynthetic_veg_percent - NonPhotosynthetic_veg_percent - Bare_soil_percent
-
-        #set up color palette
-        pal = [sns.xkcd_rgb["grey"],
-               sns.xkcd_rgb["cobalt blue"],
-               sns.xkcd_rgb["neon blue"],
-               sns.xkcd_rgb["grass"],
-               sns.xkcd_rgb["beige"],
-               sns.xkcd_rgb["brown"]]       
-        #try and figure out what the error is on one of the plots
         #make a stacked area plot
         plt.clf()
         plt.figure(figsize = (26,6))
@@ -330,27 +326,27 @@ def BigFunkyFunction(lilshape,crs):
         plt.savefig(f'{Output_dir}{polyName}.png')#, transparent=True)
         plt.show()
         print(f'plot created for {polyName}')
-
-        #make a new dataframe using the data from the xarray of wofs area for the polygon
-
-        ### start setup of dataframe by adding only one dataset
-        WOFS_df = pd.DataFrame(data=wofs_area_percent.data, index=wofs_area_percent.time.values,columns=['wofs_area_percent'])
-
-        #add data into pandas dataframe for export
-        WOFS_df['tcw_area_percent']=tcw_less_wofs.data
-        WOFS_df['PV_percent']=Photosynthetic_veg_percent.data
-        WOFS_df['NPV_percent']=NonPhotosynthetic_veg_percent.data
-        WOFS_df['BS_percent']=Bare_soil_percent.data
-        WOFS_df['Cloud_percent']=NoData.data
-
-        #call the composite dataframe something sensible, like PolyDrill
-        PolyDrill_df = WOFS_df.round(2)
-
-        #save the csv of the output data used to create the stacked plot for the polygon drill
-        PolyDrill_df.to_csv(f'{Output_dir}{polyName}.csv')
-        print(f'wrote output data to file {Output_dir}{polyName}.csv')
     except:
-        print(f'Did not run for {polyName}')
+        print("Unexpected error:",sys.exc_info()[0])
+    
+    #make a new dataframe using the data from the xarray of wofs area for the polygon
+
+    ### start setup of dataframe by adding only one dataset
+    WOFS_df = pd.DataFrame(data=wofs_area_percent.data, index=wofs_area_percent.time.values,columns=['wofs_area_percent'])
+
+    #add data into pandas dataframe for export
+    WOFS_df['tcw_area_percent']=tcw_less_wofs.data
+    WOFS_df['PV_percent']=Photosynthetic_veg_percent.data
+    WOFS_df['NPV_percent']=NonPhotosynthetic_veg_percent.data
+    WOFS_df['BS_percent']=Bare_soil_percent.data
+    WOFS_df['Cloud_percent']=NoData.data
+
+    #call the composite dataframe something sensible, like PolyDrill
+    PolyDrill_df = WOFS_df.round(2)
+
+    #save the csv of the output data used to create the stacked plot for the polygon drill
+    PolyDrill_df.to_csv(f'{Output_dir}{polyName}.csv')
+    print(f'wrote output data to file {Output_dir}{polyName}.csv')
     return 1
 
 ## __Mainline__
@@ -373,6 +369,3 @@ for shapes in shapessubset:
             print('second go did not succeed, running for last time')
             ### try for a third and last time
             result = BigFunkyFunction(shapes, crs)   
-
-
-
