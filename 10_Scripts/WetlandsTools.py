701c58a8dfeaabd560e679c931cf225b1ce5f46f
BexDunn <bex.dunn@ga.gov.au>
2019-07-03 15:06:04 +1000

    start module for WIT shapefile work

---
+++ 10_Scripts/WetlandsTools.py
@@ -0,0 +1,44 @@
+# WetlandsTools.py
+"""
+This file contains a set of python functions used for processing existing shapefile types for the Wetlands Insight Tool.  In future these will be turned into a wetlands insight tool module, with options to select a polygon type or a default setting getting the name off the OBJECTID of the polygon.
+Available functions:
+
+    get_RAMSAR_polyName
+    get_LRA_polyName
+
+Last modified: July 2019
+Author: Bex Dunn
+
+"""
+from datacube.utils import geometry
+import fiona
+import geopandas
+
+
+def get_RAMSAR_polyName(shapefile):
+    ''' function designed specifically for the RAMSAR wetlands australia shapefile. Takes the shapefile and extracts
+    the ramsar name, wetland name and objectID from the ESRI shapefile format and turns it into a useful string for our output.
+    :Inputs: shapefile with RAMSAR_NAM, WETLAND_NA, and OBJECTID as properties. 
+    Author: Bex Dunn Last Edited: March 2019'''
+    # get the ramsar name from the shapes 
+    RAMSAR_NAME = '_'.join(shapefile['properties']['RAMSAR_NAM'].split(' '))
+    WETLAND_NAME = '_'.join(shapefile['properties']['WETLAND_NA'].split(' '))
+    STATE = '_'.join(shapefile['properties']['STATE'].split(' ')) 
+    ID = shapefile['FeatureID']
+    polyName = f'{RAMSAR_NAME}-{WETLAND_NAME}-{STATE}-{ID}'
+    print(f'processing polygon {polyName}')
+    return(polyName)
+
+def get_LRA_polyName(feature):
+    ''' function designed specifically for the LRA australia geojson. Takes the geojson file  and extracts
+    the polygon name and objectID andturns it into a useful string for our output.
+    :Inputs: geojson with OBJECTID and NAME as properties. 
+    Author: Bex Dunn Last Edited: May 2019'''
+    # get name shapes 
+    #NAME = '_'.join(shpfile['properties']['NAME'].split(' '))
+    LOTPLANSEGPAR = f"{feature['properties']['LOTPLAN']}_{feature['properties']['SEGPAR']}"
+    OBJECTID = feature['properties']['OBJECTID']
+    LOCALITY = feature['properties']['LOCALITY']
+    polyName = f'{LOTPLANSEGPAR}-{LOCALITY}-{OBJECTID}'
+    print(f'processing polygon {polyName}')
+    return(polyName)
