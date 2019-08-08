6f32391b4ad105735d7e64cd82fe0625357f6cf7
Chad Burton <c.a.burton123@gmail.com>
2019-08-08 15:49:12 +1000

    adding ecological forecasting project

--- dcStats/run_dcStats.sh
+++ dcStats/run_dcStats.sh
@@ -8,7 +8,7 @@ export PYTHONPATH="/g/data1a/r78/datacube_stats/":"$PYTHONPATH"
 
 # datacube-stats --qsub="project=r78,nodes=2,walltime=5h,mem=small,queue=express" barwon_DCstats.yaml --workers-per-node=5
 
-datacube-stats --qsub="project=r78,nodes=2,walltime=2h,mem=medium,queue=express" mdbNSW_DCstats_summer.yaml --workers-per-node=10
+datacube-stats --qsub="project=r78,nodes=1,walltime=3h,mem=large,queue=expressbw" griffith_MSAVI.yaml --workers-per-node=10
 
 
 
