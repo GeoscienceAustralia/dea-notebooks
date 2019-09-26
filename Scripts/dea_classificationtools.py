52875f8d40229f773e1f0169ae5a55621e67ad0c
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2019-09-26 02:05:03 +0000

    Add skeletons for some more functionality to dea_classificationtools.py

--- Scripts/dea_classificationtools.py
+++ Scripts/dea_classificationtools.py
@@ -6,6 +6,10 @@ License: The code in this notebook is licensed under the Apache License, Version
 
 Contact: If you need assistance, please post a question on the Open Data Cube Slack channel (http://slack.opendatacube.org/) or on the GIS Stack Exchange (https://gis.stackexchange.com/questions/ask?tags=open-data-cube) using the `open-data-cube` tag (you can view previously asked questions here: https://gis.stackexchange.com/questions/tagged/open-data-cube). 
 
+Compatibility: This script is compatible with the DEA VDI environment, but incompatible with the DEA Sandbox in its current (2019-09-26) default state. This is because it requires scikit-learn to be installed, which is not the case
+               by default on the sandbox. The workaround is to run `pip install --user scikit-learn' in a terminal on the sandbox, but this may break functionality if a conflicting version of scikit-learn is installed on the sandbox
+               in the future.
+
 If you would like to report an issue with this script, you can file one on Github (https://github.com/GeoscienceAustralia/dea-notebooks/issues/new).
 
 Last modified: September 2019
@@ -17,9 +21,11 @@ Authors: Richard Taylor, Sean Chua, ...
 import numpy as np
 import xarray as xr
 
+from sklearn.cluster import KMeans
+
 #'Wrappers' to translate xarrays to np arrays and back for interfacing with sklearn models
 
-def _sklearn_flatten(input_xr):
+def sklearn_flatten(input_xr):
     """
     Reshape a DataArray or Dataset with spatial (and optionally temporal) structure into 
     an np.array with the spatial and temporal dimensions flattened into one dimension.
@@ -70,7 +76,7 @@ def _sklearn_flatten(input_xr):
     
     return input_np
 
-def _sklearn_unflatten(output_np,input_xr):
+def sklearn_unflatten(output_np,input_xr):
     
     """
     Reshape a numpy array with no 'missing' elements (NaNs) and 'flattened' spatiotemporal structure
@@ -139,3 +145,72 @@ def _sklearn_unflatten(output_np,input_xr):
     
 
     
+def fit_xr(model,input_xr):
+    """
+    Utilise our wrappers to fit a vanilla sklearn model.
+    
+    Last modified: September 2019
+    
+    Parameters
+    ----------  
+        model : a scikit-learn model or compatible object. Must have a fit() method that takes
+                numpy arrays.
+        input_xr : a DataArray or Dataset. Must have dimensions 'x' and 'y', may have dimension 'time'.
+                   
+    Returns
+    ----------
+        model : a scikit-learn model which has been fitted to the data in the pixels of input_xr.
+    
+    """
+    
+    model = model.fit(sklearn_flatten(input_xr))
+    return model
+
+def predict_xr(model,input_xr):
+    """
+    Utilise our wrappers to predict with a vanilla sklearn model.
+    
+    Last modified: September 2019
+    
+    Parameters
+    ----------  
+        model : a scikit-learn model or compatible object. Must have a predict() method that takes
+                numpy arrays.
+        input_xr : a DataArray or Dataset. Must have dimensions 'x' and 'y', may have dimension 'time'.
+                   
+    Returns
+    ----------
+        output_xr : a DataArray containing the prediction output from model with input_xr as input. Has
+        the same spatiotemporal structure as input_xr.
+    
+    """
+    
+    output_np = model.predict(sklearn_flatten(input_xr))
+    output_xr = sklearn_unflatten(output_np,input_xr)
+    
+    return output_xr
+
+def make_supervised_data(input_xr,labelled_polys):
+    """
+    Turn an input xarray and some labelled polygons into a training/testing dataset of pixels for a supervised
+    classification model.
+    
+    Last modified: September 2019
+    
+    Parameters
+    ----------  
+        input_xr : a DataArray or Dataset. Must have dimensions 'x' and 'y', may have dimension 'time'.
+        training_polys : 
+                   
+    Returns
+    ----------
+    
+    """
+    pass
+
+class KMeans_tree():
+    """
+    A hierarchical KMeans unsupervised clustering model. This class is designed to be compatible with methods and kwargs from
+    the sklearn.cluster.KMeans() class
+    """
+    pass
