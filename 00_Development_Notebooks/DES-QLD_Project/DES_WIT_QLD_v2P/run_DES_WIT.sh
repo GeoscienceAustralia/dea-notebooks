245b4128ebe873e6918fbf55fd1eccb3e862e58b
BexDunn <bex.dunn@ga.gov.au>
2019-10-08 16:24:40 +1100

    fixed runs for QLD palustrine: removed the extra .png, fixed the qsub so that only asking for 1 node on megamem, fixed the hardcoded chunksize

---
+++ 00_Development_Notebooks/DES-QLD_Project/DES_WIT_QLD_v2P/run_DES_WIT.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#PBS -P r78 
+#PBS -q megamem 
+#PBS -N WIT_P
+#PBS -l walltime=24:00:00
+#PBS -l mem=3TB
+#PBS -l jobfs=900GB
+#PBS -l ncpus=32
+#PBS -l wd
+#PBS -M bex.dunn@ga.gov.au
+#PBS -m abe
+
+#NNODES=2
+NNODES=$(cat $PBS_NODEFILE | uniq | wc -l)
+NCPUS=32 #number of cpus per node in this queue
+NCHUNKS=270 #number of chunks in script
+JOBDIR=$PWD
+
+for i in $(seq 0 $(($NNODES-1))); do
+    if [ $i -lt 1 ]
+    then
+        PARAMF="{1..134},$NCHUNKS"
+    else 
+        PARAMF="{135..270},$NCHUNKS" 
+    fi
+    pbsdsh -n $(( $NCPUS*$i )) -- \ 
+    bash -l -c "\
+    module use /g/data/v10/public/modules/modulefiles/;\
+    module load dea;\
+    module load parallel;\
+    echo $i:\
+    cd $JOBDIR;\
+    parallel --delay 5 --retries 3 --load 100%  --colsep ',' python /g/data/r78/rjd547/jupyter_notebooks/dea-notebooks/00_Development_Notebooks/DES-QLD_Project/DES_WIT_QLD_v2P/DES_WIT_raijinP.py ::: $PARAMF"&
+done;
+wait;
