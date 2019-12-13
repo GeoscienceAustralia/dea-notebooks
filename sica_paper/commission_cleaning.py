10e7e10959c106c5941759167668d3281a92a47c
Chad Burton <c.a.burton123@gmail.com>
2019-12-13 20:20:40 +1100

    updates to commissions error script

--- sica_paper/commission_cleaning.py
+++ sica_paper/commission_cleaning.py
@@ -52,19 +52,6 @@ def geotransform(ds, coords, epsg=3577, alignment = 'centre', rotation=0.0):
     
     return transform, prj_wkt
 
-def convertIrrShpToTiff(shp, year):  
-    #open a tif and get transform info
-    tif = shp[:77]+shp[77:95]+"_multithreshold_65Thres"+suffix[10:]+".tif"
-    ds = xr.open_rasterio(tif).squeeze()
-    transform, proj = geotransform(ds, (ds.x, ds.y), epsg=3577)
-    rows,cols = ds.shape
-    #turn vector into numpy array
-    shp_arr = rasterize_vector(shp, cols=cols, rows=rows, geo_transform=transform, projection=proj)
-    #convert numpy array into xarray
-    shp_xr = xr.DataArray(shp_arr, coords = [ds.y, ds.x], dims = ['y', 'x'])
-    #append xarray to list
-    da_list.append(shp_xr)
-
 #create parallized function for calculting sum and nanargmax
 def count_irrigation(x, dim):
     return xr.apply_ufunc(np.sum, x, dask='parallelized',
@@ -84,35 +71,6 @@ def IrrigationFirstOccurs(x, dim):
 
 
 #-----------SCRIPT-----------------
-#list of years to help for-loop iterate through folders
-x = range(1987,2019,1)
-years = []
-for i in x:
-    nextyear = str(i + 1)[2:]
-    y = str(i) + "_" + nextyear
-    years.append(str(y))
-# removing years that didn't work
-years =  [e for e in years if e not in ('2011_12', '2012_13')]
-years.sort()
-
-#list of folders to help with loop
-folders = os.listdir(directory)
-folders.sort()
-
-da_list = []
-for year, folder in zip(years, folders): 
-    print("\r", "working on year: " + year, end = '')
-    convertIrrShpToTiff(directory+folder+"/"+"nmdb_Summer"+ year + suffix+".shp", year)
-
-#generate date ranges to use as coordinates in xrray dataset
-dates = pd.date_range(start='1/1/1987', end='1/01/2019', freq='Y')
-dates = dates.drop([pd.Timestamp('2011-12-31'), pd.Timestamp('2012-12-31')])
-#concatenate all xarrays into a single multi-dim xarray with time ('dates') as coords.
-da = xr.concat(da_list, dim=dates).rename({'concat_dim':'time'}).rename('Irrigated_Area')
-#convert to dataset
-ds = da.to_dataset()
-#export as netcdf
-ds.to_netcdf(results + "NMDB_irrigation.nc")
 
 #bring in data
 irr_alltime = xr.open_dataset(results+'NMDB_irrigation.nc').astype(bool)
