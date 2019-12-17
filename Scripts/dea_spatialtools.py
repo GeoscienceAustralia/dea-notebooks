b8b4cca23b6e73b5e8db05e38f601c1e40bd14e9
Claire <claire.krause@ga.gov.au>
2019-12-17 14:52:02 +1100

    Add functions to Scripts, fix tags in ITEM coastal notebook

--- Scripts/dea_spatialtools.py
+++ Scripts/dea_spatialtools.py
@@ -22,6 +22,8 @@ Functions included:
     interpolate_2d
     contours_to_array
     largest_region
+    array_to_geotiff
+    geotransform
 
 Last modified: November 2019
 
@@ -411,3 +413,129 @@ def largest_region(bool_array, **kwargs):
     
     return largest_region
 
+def array_to_geotiff(fname, data, geo_transform, projection,
+                     nodata_val=0, dtype=gdal.GDT_Float32):
+    """
+    Create a single band GeoTIFF file with data from an array. 
+    
+    Because this works with simple arrays rather than xarray datasets from DEA, it requires
+    geotransform info ("(upleft_x, x_size, x_rotation, upleft_y, y_rotation, y_size)") and 
+    projection data (in "WKT" format) for the output raster. These are typically obtained from 
+    an existing raster using the following GDAL calls:
+    
+        import gdal
+        gdal_dataset = gdal.Open(raster_path)
+        geotrans = gdal_dataset.GetGeoTransform()
+        prj = gdal_dataset.GetProjection()
+    
+    ...or alternatively, directly from an xarray dataset:
+    
+        geotrans = xarraydataset.geobox.transform.to_gdal()
+        prj = xarraydataset.geobox.crs.wkt
+    
+    Last modified: March 2018
+    Author: Robbi Bishop-Taylor
+    
+    :param fname: 
+        Output geotiff file path including extension
+        
+    :param data: 
+        Input array to export as a geotiff
+        
+    :param geo_transform: 
+        Geotransform for output raster; e.g. "(upleft_x, x_size, x_rotation, 
+        upleft_y, y_rotation, y_size)"
+        
+    :param projection:
+        Projection for output raster (in "WKT" format)
+        
+    :param nodata_val: 
+        Value to convert to nodata in the output raster; default 0
+        
+    :param dtype: 
+        Optionally set the dtype of the output raster; can be useful when exporting 
+        an array of float or integer values. Defaults to gdal.GDT_Float32
+        
+    """
+
+    # Set up driver
+    driver = gdal.GetDriverByName('GTiff')
+
+    # Create raster of given size and projection
+    rows, cols = data.shape
+    dataset = driver.Create(fname, cols, rows, 1, dtype)
+    dataset.SetGeoTransform(geo_transform)
+    dataset.SetProjection(projection)
+
+    # Write data to array and set nodata values
+    band = dataset.GetRasterBand(1)
+    band.WriteArray(data)
+    band.SetNoDataValue(nodata_val)
+
+    # Close file
+    dataset = None
+
+def geotransform(ds, coords, epsg=3577, alignment = 'centre', rotation=0.0):
+    """
+    Creates a GDAL geotransform tuple from an xarray object, along with
+    a projection object in the form of WKT. Basically provides everything you need to use
+    the 'array_to_geotiff' function from '10_Scripts/SpatialTools', and that is the express
+    purpose for this function.
+    
+    :param ds:
+        xarray dataset or dataArray object
+    :param coords:
+        Tuple. The georeferencing coordinate data in the xarray object. 
+        e.g (ds.long,ds.lat) or (ds.x,ds.y). Order MUST BE X then Y
+    :param epsg:
+        Integer. A projection number in epsg format, defaults to 3577 (albers equal area).
+    :param pixelSize:
+        float. The size of the pixels in metres. defaults to 25m, as per Landsat pixel size
+    :param alignment:
+        Str. How should the coords be aligned with respect to the pixels? 
+        If "centre", then the transform will align coordinates with the centre of the pixel.
+        If 'upper_left', then coords will be aligned with the upperleftmost corner of array
+    :param rotation:
+        Float. the degrees of rotation of the image. If North is up, rotation = 0.0.
+    
+        Example:
+        #Open an xarray object
+        ds = xr.open_rasterio(input_file.tif).squeeze()
+        #grab the trasnform and projection info from the dataset
+        transform, projection = geotransform(ds, (ds.x, ds.y), epsg=3577)
+        #use the transform object in a 'array_to_geotiff' function
+        width,height = a.shape
+        SpatialTools.array_to_geotiff("output_file.tif",
+          ds.values, geo_transform = transform, 
+          projection = projection, 
+          nodata_val=-999)
+    
+      :Returns:
+          A tuple containing the geotransform tuple and WKT projection information
+          i.e. (transform_tuple, projection)
+      -------------------------------------------------------------------------    
+    """
+    print("This function is written for use with the GDAL backed 'array_to_geotiff' function and should be used with extreme caution elsewhere.")
+    
+    
+    if alignment == 'centre':
+        EW_pixelRes = float(coords[1][0] - coords[1][1])
+        NS_pixelRes = float(coords[0][0] - coords[0][1])        
+        east = float(coords[0][0]) - (EW_pixelRes/2)
+        north = float(coords[1][0]) + (NS_pixelRes/2)
+        
+        transform = (east, EW_pixelRes, rotation, north, rotation, NS_pixelRes)
+    
+    elif alignment == 'upper_left':
+        EW_pixelRes = float(coords[1][0] - coords[1][1])
+        NS_pixelRes = float(coords[0][0] - coords[0][1])        
+        east = float(coords[0][0])
+        north = float(coords[1][0])
+        
+        transform = (east, EW_pixelRes, rotation, north, rotation, NS_pixelRes)
+    
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(epsg)
+    prj_wkt = srs.ExportToWkt()
+    
+    return transform, prj_wkt
