66a69637209841a82814d1d2d38062219f57939f
Katey Dugdale <katey.dugdale@ga.gov.au>
2020-02-05 05:00:24 +0000

    adding a folder for the OzEWEX course

---
+++ for_MDBA/Scripts/dea_spatialtools.py
@@ -0,0 +1,13 @@
+## dea_spatialtools.py
+'''
+Description: This file contains a set of python functions for conducting spatial analyses on Digital Earth Australia data.
+
+License: The code in this notebook is licensed under the Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0). Digital Earth Australia data is licensed under the Creative Commons by Attribution 4.0 license (https://creativecommons.org/licenses/by/4.0/).
+
+Contact: If you need assistance, please post a question on the Open Data Cube Slack channel (http://slack.opendatacube.org/) or on the GIS Stack Exchange (https://gis.stackexchange.com/questions/ask?tags=open-data-cube) using the `open-data-cube` tag (you can view previously asked questions here: https://gis.stackexchange.com/questions/tagged/open-data-cube). 
+
+If you would like to report an issue with this script, you can file one on Github (https://github.com/GeoscienceAustralia/dea-notebooks/issues/new).
+
+Last modified: September 2019
+
+'''
