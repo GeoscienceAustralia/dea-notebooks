f7c4df72e35d3e150018e81406f509c09c55c4a0
cbur24 <c.a.burton123@gmail.com>
2020-02-07 00:46:11 +0000

    furthe edits to markdown and print statements

--- Scripts/dea_spatialtools.py
+++ Scripts/dea_spatialtools.py
@@ -288,10 +288,10 @@ def xr_rasterize(gdf,
     
     if export_tiff:
         try:
-            print("exporting geotiff with array name: " + name)
+            print("Exporting GeoTIFF with array name: " + name)
             write_geotiff(export_tiff, xarr.to_dataset(name = name)) 
         except:
-            print("exporting geotiff with default array name: 'data'")
+            print("Exporting GeoTIFF with default array name: 'data'")
             write_geotiff(export_tiff, xarr.to_dataset(name = 'data'))
             
     return xarr
