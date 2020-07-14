3da7f1883479092c9d769ff26166397e4be29a06
S M T Chua <sean.m.t.chua@gmail.com>
2020-07-14 14:35:37 +1000

    Update dea_classificationtools.py

--- Scripts/dea_classificationtools.py
+++ Scripts/dea_classificationtools.py
@@ -1,28 +1,19 @@
-# deafrica_classificationtools.py
+# dea_classificationtools.py
 '''
-Description: This file contains a set of python functions for conducting
-machine learning classification on remote sensing data from Digital Earth 
-Africa's Open Data Cube
-
-License: The code in this notebook is licensed under the Apache License, 
-Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0). Digital Earth 
-Africa data is licensed under the Creative Commons by Attribution 4.0 
+Description: This file contains a set of python functions for handling
+Digital Earth Australia data.
+License: The code in this notebook is licensed under the Apache License,
+Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0). Digital Earth
+Australia data is licensed under the Creative Commons by Attribution 4.0
 license (https://creativecommons.org/licenses/by/4.0/).
-
-Contact: If you need assistance, please post a question on the Open Data 
-Cube Slack channel (http://slack.opendatacube.org/) or on the GIS Stack 
-Exchange (https://gis.stackexchange.com/questions/ask?tags=open-data-cube) 
-using the `open-data-cube` tag (you can view previously asked questions 
+Contact: If you need assistance, please post a question on the Open Data
+Cube Slack channel (http://slack.opendatacube.org/) or on the GIS Stack
+Exchange (https://gis.stackexchange.com/questions/ask?tags=open-data-cube)
+using the `open-data-cube` tag (you can view previously asked questions
 here: https://gis.stackexchange.com/questions/tagged/open-data-cube).
-
-If you would like to report an issue with this script, you can file one on 
-Github https://github.com/digitalearthafrica/deafrica-sandbox-notebooks/issues
-
-Last modified: April 2020
-
-
+If you would like to report an issue with this script, you can file one on
+Github (https://github.com/GeoscienceAustralia/dea-notebooks/issues/new).
 '''
-
 
 from dea_spatialtools import xr_rasterize
 from dea_bandindices import calculate_indices
