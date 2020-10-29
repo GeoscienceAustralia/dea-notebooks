eddde70ebc225a41e991dd0cb5567f720ae5d4d7
robbibt <Robbi.BishopTaylor@ga.gov.au>
2020-10-29 00:57:44 +0000

    Rename script to dea_climate

--- Scripts/dea_climate.py
+++ Scripts/dea_climate.py
@@ -1,12 +1,13 @@
-# dea_load_era5.py
+# dea_climate.py
 '''
-Description: A set of python functions to retrieve ERA5 gridded climiate data.
+Description: A set of python functions to retrieve and manipulate 
+gridded climate data.
 
-Adpated from scripts by Andrew Cherry and Brian Killough.
+Adapted from scripts by Andrew Cherry and Brian Killough.
 
 License: The code in this notebook is licensed under the Apache License,
 Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0). Digital Earth 
-Australua data is licensed under the Creative Commons by Attribution 4.0 
+Australia data is licensed under the Creative Commons by Attribution 4.0 
 license (https://creativecommons.org/licenses/by/4.0/).
 
 Contact: If you need assistance, please post a question on the Open Data 
@@ -22,10 +23,9 @@ Functions included:
     get_era5_daily
     era5_area_crop
     era5_area_nearest
-    load_era5
+    load_era5    
     
-    
-Last modified: April 2020
+Last modified: October 2020
 
 '''
 
@@ -67,35 +67,43 @@ def get_era5_daily(var,
                    cache_dir='era5',
                    resample='1D'):
     """
-    Download and return an ERA5 variable for a defined time window.
+    Download and return an variable from the European Centre for Medium 
+    Range Weather Forecasts (ECMWF) global climate reanalysis product 
+    (ERA5) for a defined time window.
 
     Parameters
     ----------     
     var : string
-        Name of the ERA5 climate variable to download, e.g "air_temperature_at_2_metres" 
+        Name of the ERA5 climate variable to download, e.g 
+        "air_temperature_at_2_metres" 
 
     date_from_arg: string or datetime object
         Starting date of the time window.
         
     date_to_arg: string or datetime object
-        End date of the time window. If not supplied, set to be the same as starting date.
+        End date of the time window. If not supplied, set to be the same
+        as starting date.
 
     reduce_func: numpy function
         lets you specify a function to apply to each day's worth of data.  
-        The default is np.mean, which computes daily average. To get a sum, use np.sum.
+        The default is np.mean, which computes daily average. To get a 
+        sum, use np.sum.
 
     cache_dir: sting
-        Path to save downloaded ERA5 data. The path will be created if not already exists.
+        Path to save downloaded ERA5 data. The path will be created if 
+        not already exists.
         The default is 'era5'.
         
     resample: string
-        Temporal resampling frequency to be used for xarray's resample function.
-        The default is '1D', which is daily.
-        Since ERA5 data is provided as one file per month, maximum resampling period is '1M'.
+        Temporal resampling frequency to be used for xarray's resample
+        function. The default is '1D', which is daily. Since ERA5 data 
+        is provided as one file per month, maximum resampling period is 
+        '1M'.
 
     Returns
     -------
-    A lazy-loaded xarray dataset containing an ERA5 variable for the selected time window.
+    A lazy-loaded xarray dataset containing an ERA5 variable for the 
+    selected time window.
 
     """
 
@@ -112,9 +120,11 @@ def get_era5_daily(var,
         date_to_arg = parse(date_to_arg)
     if date_to_arg is None:
         date_to_arg = date_from_arg
+        
     # Make sure our dates are in the correct order
     from_date = min(date_from_arg, date_to_arg)
     to_date = max(date_from_arg, date_to_arg)
+    
     # Download ERA5 files to local cache if they don't already exist
     client = None  # Boto client (if needed)
     local_files = []  # Will hold list of local filenames
@@ -126,7 +136,7 @@ def get_era5_daily(var,
         data_key = "{Y:04}/{M:02}/data/{var}.nc".format(Y=Y, M=M, var=var)
         if not os.path.isfile(
                 local_file
-        ):  # check if file already exists (TODO: move to temp?) (TODO: catch failed download)
+        ):  # check if file already exists (TODO: move to temp, catch failed download)
             if client is None:
                 client = boto3.client('s3',
                                       config=botocore.client.Config(
@@ -138,6 +148,7 @@ def get_era5_daily(var,
             M = 1
         else:
             M += 1
+            
     # Load and merge the locally-cached ERA5 data from the list of filenames
     date_slice = slice(str(from_date.date()), str(to_date.date(
     )))  # I do this to INCLUDE the whole end date, not just 00:00
@@ -166,8 +177,13 @@ def get_era5_daily(var,
 
 def era5_area_crop(ds, lat, lon):
     """
-    Crop a dataset containing EAR5 variables to a location. 
-    The output spatial grid will either include input grid points within lat/lon boundaries or the nearest point if none is within the search location.  
+    Crop a dataset containing European Centre for Medium Range Weather 
+    Forecasts (ECMWF) global climate reanalysis product (ERA5) variables
+    to a location. 
+    
+    The output spatial grid will either include input grid points within 
+    lat/lon boundaries or the nearest point if none is within the search
+    location.  
 
     Parameters
     ----------     
@@ -182,7 +198,8 @@ def era5_area_crop(ds, lat, lon):
 
     Returns
     -------
-    An xarray dataset containing ERA5 variables for the selected location.
+    An xarray dataset containing ERA5 variables for the selected 
+    location.
 
     """
     
@@ -191,14 +208,17 @@ def era5_area_crop(ds, lat, lon):
         min(lat)
     except TypeError:
         lat = [lat]
+        
     try:
         min(lon)
     except TypeError:
         lon = [lon]
+        
     if min(lon) < 0:
         # re-order along longitude to go from -180 to 180
         ds = ds.assign_coords({"lon": (((ds.lon + 180) % 360) - 180)})
         ds = ds.reindex({ "lon": np.sort(ds.lon)})
+        
     # Issue warnings if args outside range.
     if min(lat) < ds.lat.min() or max(lat) > ds.lat.max():
         warnings.warn("Lats must be in range {} .. {}.  Got: {}".format(
@@ -208,9 +228,11 @@ def era5_area_crop(ds, lat, lon):
         warnings.warn("Lons must be in range {} .. {}.  Got: {}".format(
             ds.lon.min().values,
             ds.lon.max().values, lon))
+        
     # Find existing coords between min&max
     lats = ds.lat[np.logical_and(
         ds.lat >= min(lat), ds.lat <= max(lat))].values
+    
     # If there was nothing between, just plan to grab closest
     if len(lats) == 0:
         lats = np.unique(ds.lat.sel(lat=np.array(lat), method="nearest"))
@@ -218,6 +240,7 @@ def era5_area_crop(ds, lat, lon):
         ds.lon >= min(lon), ds.lon <= max(lon))].values
     if len(lons) == 0:
         lons = np.unique(ds.lon.sel(lon=np.array(lon), method="nearest"))
+        
     # crop and keep attrs
     output = ds.sel(lat=lats, lon=lons)
     output.attrs = ds.attrs
@@ -228,7 +251,10 @@ def era5_area_crop(ds, lat, lon):
 
 def era5_area_nearest(ds, lat, lon):
     """
-    Crop a dataset containing EAR5 variables to a location. 
+    Crop a dataset containing European Centre for Medium 
+    Range Weather Forecasts (ECMWF) global climate reanalysis product 
+    (ERA5) variables to a location. 
+    
     The output spatial grid is snapped to the nearest input grid points.  
 
     Parameters
@@ -255,12 +281,15 @@ def era5_area_nearest(ds, lat, lon):
         
     # find the nearest lat lon boundary points
     test = ds.sel(lat=lat, lon=lon, method='nearest')
+    
     # define the lat/lon grid
     lat_range = slice(test.lat.max().values, test.lat.min().values)
     lon_range = slice(test.lon.min().values, test.lon.max().values)
+    
     # crop and keep attrs
     output = ds.sel(lat=lat_range, lon=lon_range)
     output.attrs = ds.attrs
+    
     for var in output.data_vars:
         output[var].attrs = ds[var].attrs
     return output
@@ -268,12 +297,15 @@ def era5_area_nearest(ds, lat, lon):
 
 def load_era5(var, lat, lon, time, grid='nearest', **kwargs):
     """
-    Returns a ERA5 variable for a selected location and time window. 
+    Returns a European Centre for Medium Range Weather Forecasts (ECMWF)
+    global climate reanalysis product (ERA5) variable for a selected 
+    location and time window. 
 
     Parameters
     ----------     
     var : string
-        Name of the ERA5 climate variable to download, e.g "air_temperature_at_2_metres" 
+        Name of the ERA5 climate variable to download, e.g 
+        "air_temperature_at_2_metres" 
 
     lat: tuple or list
         Latitude range for query.
@@ -286,12 +318,16 @@ def load_era5(var, lat, lon, time, grid='nearest', **kwargs):
     
     grid: string
         Option for output spatial gridding.
-        The default is 'nearest', for which output spatial grid is snapped to the nearest ERA5 input grid points.
-        Alternatively, output spatial grid will either include input grid points within lat/lon boundaries or the nearest point if none is within the search location. 
+        The default is 'nearest', for which output spatial grid is 
+        snapped to the nearest ERA5 input grid points.
+        Alternatively, output spatial grid will either include input 
+        grid points within lat/lon boundaries or the nearest point if 
+        none is within the search location. 
         
     Returns
     -------
-    An xarray dataset containing the variable for the selected location and time window.
+    An xarray dataset containing the variable for the selected location
+    and time window.
 
     """
 
