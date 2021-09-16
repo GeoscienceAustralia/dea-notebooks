dfd7acead50c8b2a501f787701d2af31ae21e0f9
BexDunn <bex.dunn@ga.gov.au>
2021-09-16 05:35:57 +0000

    one test for 07

--- Tests/Beginners_guide/07_Intro_to_numpy_test.py
+++ Tests/Beginners_guide/07_Intro_to_numpy_test.py
@@ -5,7 +5,6 @@ from testbook import testbook
 TEST_DIR = Path(__file__).parent.parent.resolve()
 NB_DIR = TEST_DIR.parent
 
-
 @testbook(NB_DIR / 'Beginners_guide' / '01_Jupyter_notebooks.ipynb', execute=True)
-def test_time(tb):
-    assert True  # ok
+def test_ok(tb):
+    assert True  # ok      
