6a02072d1760ef53ea8c9b8a4750b113943b0d52
BexDunn <bex.dunn@ga.gov.au>
2021-09-16 05:49:07 +0000

    runs tests on 02

--- Tests/Beginners_guide/02_DEA_test.py
+++ Tests/Beginners_guide/02_DEA_test.py
@@ -6,6 +6,6 @@ TEST_DIR = Path(__file__).parent.parent.resolve()
 NB_DIR = TEST_DIR.parent
 
 
-@testbook(NB_DIR / 'Beginners_guide' / '01_Jupyter_notebooks.ipynb', execute=True)
+@testbook(NB_DIR / 'Beginners_guide' / '02_DEA.ipynb', execute=True)
 def test_ok(tb):
     assert True  # ok
