f73e47647c20bede1424745c5aa7c8f718f32462
Matthew Alger <matthew.alger@ga.gov.au>
2021-09-16 04:55:29 +0000

    Added test for beginners guide

--- Tests/Beginners_guide/01_Jupyter_notebooks_test.py
+++ Tests/Beginners_guide/01_Jupyter_notebooks_test.py
@@ -1,3 +1,11 @@
-import subprocess
+from pathlib import Path
 
-import pytest
+from testbook import testbook
+
+TEST_DIR = Path(__file__).parent.parent.resolve()
+NB_DIR = TEST_DIR.parent
+
+
+@testbook(NB_DIR / 'Beginners_guide' / '01_Jupyter_notebooks.ipynb', execute=True)
+def test_time(tb):
+    assert True  # ok
