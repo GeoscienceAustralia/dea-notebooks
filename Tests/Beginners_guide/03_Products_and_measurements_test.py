15e6b581419c3125532fda43077dfb53d4c25453
BexDunn <bex.dunn@ga.gov.au>
2021-09-16 05:58:50 +0000

    point to the correct nb

--- Tests/Beginners_guide/03_Products_and_measurements_test.py
+++ Tests/Beginners_guide/03_Products_and_measurements_test.py
@@ -6,6 +6,6 @@ TEST_DIR = Path(__file__).parent.parent.resolve()
 NB_DIR = TEST_DIR.parent
 
 
-@testbook(NB_DIR / 'Beginners_guide' / '02_DEA.ipynb', execute=True)
+@testbook(NB_DIR / 'Beginners_guide' / '03_Products_and_measurements.ipynb', execute=True)
 def test_ok(tb):
     assert True  # ok
