8ff5b9d83ff0054455399e19e7d4347bdc4a6d6f
BexDunn <bex.dunn@ga.gov.au>
2021-09-16 05:43:27 +0000

    create 02_DEA_test.py as a copy of 01

---
+++ Tests/Beginners_guide/02_DEA_test.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from testbook import testbook
+
+TEST_DIR = Path(__file__).parent.parent.resolve()
+NB_DIR = TEST_DIR.parent
+
+
+@testbook(NB_DIR / 'Beginners_guide' / '01_Jupyter_notebooks.ipynb', execute=True)
+def test_ok(tb):
+    assert True  # ok
