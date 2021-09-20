174d9629314c4d58c9bbf65b48828a2b69924716
Matthew Alger <matthew.alger@gmail.com>
2021-09-20 15:30:47 +1000

    Update setup_test_datacube.sh

--- Tests/setup_test_datacube.sh
+++ Tests/setup_test_datacube.sh
@@ -2,13 +2,13 @@
 set -ex
 
 # Setup datacube
-docker-compose exec -T index datacube system init --no-default-types --no-init-users
+datacube system init --no-default-types --no-init-users
 # Setup metadata types
-docker-compose exec -T index datacube metadata add "$METADATA_CATALOG"
-# Index products we care about for dea-waterbodies
-docker-compose exec -T index wget "$PRODUCT_CATALOG" -O product_list.csv
+datacube metadata add "$METADATA_CATALOG"
+# Index products we care about for dea-notebooks
+wget "$PRODUCT_CATALOG" -O product_list.csv
 
-docker-compose exec -T index bash -c "tail -n+2 product_list.csv | grep 'ls7_nbart_geomedian_annual\|ls8_nbart_geomedian_annual\|ga_ls_wo_3\|s2a_ard_granule\|s2b_ard_granule\|ga_ls5t_ard_3\|ga_ls7e_ard_3\|ga_ls8c_ard_3\|wofs_annual_summary' | awk -F , '{print \$2}' | xargs datacube -v product add" 
+bash -c "tail -n+2 product_list.csv | grep 'ls7_nbart_geomedian_annual\|ls8_nbart_geomedian_annual\|ga_ls_wo_3\|s2a_ard_granule\|s2b_ard_granule\|ga_ls5t_ard_3\|ga_ls7e_ard_3\|ga_ls8c_ard_3\|wofs_annual_summary' | awk -F , '{print \$2}' | xargs datacube -v product add" 
 
 # Index scenes
 cat > index_tiles.sh <<EOF
@@ -20,4 +20,4 @@ s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L8/x_20/y_-32/2016/01/
 s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L8/x_20/y_-32/2017/01/01/*.yaml' --no-sign-request --skip-lineage 'ls8_nbart_geomedian_annual'
 EOF
 
-cat index_tiles.sh | docker-compose exec -T index bash
+cat index_tiles.sh | bash
