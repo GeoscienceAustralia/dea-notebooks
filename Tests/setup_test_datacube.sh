c18d607ef1104266019e8b2fad6ee5a24cd3d8c7
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2021-09-28 16:50:56 +1000

    Add first set of basic Testbook tests to DEA Notebooks (#872)
    * Update and rename test-notebooks.yml to test_notebooks.yml
    
    * Update test_notebooks.yml
    
    * Update test_notebooks.yml
    
    * Update test_notebooks.yml
    
    * Update test_notebooks.yml
    
    * Update test_notebooks.yml
    
    * Added test for beginners guide
    
    * create testing notebook for no 7
    
    * Updated tests
    
    * Create docker-compose.yml
    
    * Add env vars
    
    * one test for 07
    
    * Update test_notebooks.yml
    
    * create 02_DEA_test.py as a copy of 01
    
    * Update test_notebooks.yml
    
    * runs tests on 02
    
    * random typo broke 02; fixed
    
    * copied 02 to 03 test
    
    * point to the correct nb
    
    * added postgres and index containers
    
    * created loading data test file
    
    * Add new frequently used code tests
    
    * Fix relative file path errors in contour extraction
    
    * Minor updates to notebooks to improve tests
    
    * Add additional tests
    
    * Fix relative paths, standardise test format using Black
    
    * Update test_notebooks.yml
    
    * Index geomedians
    
    * Update setup_test_datacube.sh
    
    * Update test_notebooks.yml
    
    * Added sandbox image
    
    * Added volume mount
    
    * Docker compose
    
    * Added metadata and product catalogues to index
    
    * Update test_notebooks.yml
    
    * Update test_notebooks.yml
    
    * Installed testbook
    
    * Install local dea-tools
    
    * in-tree-build for dea-tools
    
    * Copy Tools to writable directory before build
    
    * Explicit home dir for Tools
    
    * Update test_notebooks.yml
    
    * Added scene getter script
    
    * Added dask "support"
    
    * Patched arbitary functions
    
    * Sorted and removed duplicates
    
    * Add scenes for 06_Basic_analysis_test
    
    * Temporarily comment out failing tests and indexing paths
    
    * Revert commenting out
    
    * Ignore frequently used code and 06_Basic_analysis_test.py
    
    * Also remove intro to numpy test due to relative link issue
    
    * Use correct file name
    
    Co-authored-by: Matthew Alger <matthew.alger@gmail.com>
    Co-authored-by: Matthew Alger <matthew.alger@ga.gov.au>
    Co-authored-by: BexDunn <bex.dunn@ga.gov.au>

---
+++ Tests/setup_test_datacube.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -ex
+
+# Setup datacube
+datacube system init --no-default-types --no-init-users
+# Setup metadata types
+datacube metadata add "$METADATA_CATALOG"
+# Index products we care about for dea-notebooks
+wget "$PRODUCT_CATALOG" -O product_list.csv
+
+bash -c "tail -n+2 product_list.csv | grep 'ls7_nbart_geomedian_annual\|ls8_nbart_geomedian_annual\|ga_ls_wo_3\|s2a_ard_granule\|s2b_ard_granule\|ga_ls5t_ard_3\|ga_ls7e_ard_3\|ga_ls8c_ard_3\|wofs_annual_summary' | awk -F , '{print \$2}' | xargs datacube -v product add" 
+
+# Index scenes
+cat > index_tiles.sh <<EOF
+s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L7/x_20/y_-32/2015/01/01/*.yaml' --no-sign-request --skip-lineage 'ls7_nbart_geomedian_annual'
+s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L8/x_20/y_-32/2013/01/01/*.yaml' --no-sign-request --skip-lineage 'ls8_nbart_geomedian_annual'
+s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L8/x_20/y_-32/2014/01/01/*.yaml' --no-sign-request --skip-lineage 'ls8_nbart_geomedian_annual'
+s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L8/x_20/y_-32/2015/01/01/*.yaml' --no-sign-request --skip-lineage 'ls8_nbart_geomedian_annual'
+s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L8/x_20/y_-32/2016/01/01/*.yaml' --no-sign-request --skip-lineage 'ls8_nbart_geomedian_annual'
+s3-to-dc 's3://dea-public-data/geomedian-australia/v2.1.0/L8/x_20/y_-32/2017/01/01/*.yaml' --no-sign-request --skip-lineage 'ls8_nbart_geomedian_annual'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-09-09/S2A_OPER_MSI_ARD_TL_EPAE_20180909T020622_A016787_T55LBD_N02.06/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-09-19/S2A_OPER_MSI_ARD_TL_EPAE_20180919T021041_A016930_T55LBD_N02.06/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-09-29/S2A_OPER_MSI_ARD_TL_EPAE_20180929T020742_A017073_T55LBD_N02.06/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-10-09/S2A_OPER_MSI_ARD_TL_EPAE_20181009T021157_A017216_T55LBD_N02.06/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-10-19/S2A_OPER_MSI_ARD_TL_EPAE_20181019T021416_A017359_T55LBD_N02.06/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-10-29/S2A_OPER_MSI_ARD_TL_EPAE_20181029T021107_A017502_T55LBD_N02.06/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-11-08/S2A_OPER_MSI_ARD_TL_EPAE_20181108T021044_A017645_T55LBD_N02.07/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-11-18/S2A_OPER_MSI_ARD_TL_EPAE_20181118T021103_A017788_T55LBD_N02.07/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+# s3-to-dc 's3://dea-public-data/baseline/s2a_ard_granule/2018-11-28/S2A_OPER_MSI_ARD_TL_EPAE_20181128T020701_A017931_T55LBD_N02.07/*.yaml' --no-sign-request --skip-lineage 's2a_ard_granule'
+EOF
+
+cat index_tiles.sh | bash
