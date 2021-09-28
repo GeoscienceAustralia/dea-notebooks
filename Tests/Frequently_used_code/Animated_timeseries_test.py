036e8af2c98c4c35c261567156bc568eb479fdf9
robbibt <Robbi.BishopTaylor@ga.gov.au>
2021-09-28 06:20:50 +0000

    Revert commenting out

--- Tests/Frequently_used_code/Animated_timeseries_test.py
+++ Tests/Frequently_used_code/Animated_timeseries_test.py
@@ -1,52 +1,52 @@
-# import os
-# import pytest
-# from pathlib import Path
-# from testbook import testbook
+import os
+import pytest
+from pathlib import Path
+from testbook import testbook
 
-# TEST_DIR = Path(__file__).parent.parent.resolve()
-# NB_DIR = TEST_DIR.parent
-# NB_PATH = NB_DIR / "Frequently_used_code" / "Animated_timeseries.ipynb"
-
-
-# @pytest.fixture(scope="module")
-# def tb():
-
-#     # Update working directory to ensure relative links in notebooks work
-#     os.chdir(NB_DIR.parent)
-
-#     with testbook(NB_PATH, execute=True) as tb:
-#         yield tb
+TEST_DIR = Path(__file__).parent.parent.resolve()
+NB_DIR = TEST_DIR.parent
+NB_PATH = NB_DIR / "Frequently_used_code" / "Animated_timeseries.ipynb"
 
 
-# def test_ok(tb):
-#     assert True  # ok
+@pytest.fixture(scope="module")
+def tb():
+
+    # Update working directory to ensure relative links in notebooks work
+    os.chdir(NB_DIR.parent)
+
+    with testbook(NB_PATH, execute=True) as tb:
+        yield tb
 
 
-# def test_cols(tb):
-#     gdf = tb.ref("poly_gdf")
-#     expected_cols = ["attribute",
-#                      "area",
-#                      "geometry",
-#                      "color",
-#                      "start_time",
-#                      "end_time"]
-#     for col in expected_cols:
-#         assert col in gdf.columns
+def test_ok(tb):
+    assert True  # ok
 
 
-# def test_vars(tb):
-#     ds = tb.ref("ds")
-#     expected_vars = [
-#         "time",
-#         "y",
-#         "x",
-#         "spatial_ref",
-#         "nbart_red",
-#         "nbart_green",
-#         "nbart_blue",
-#         "nbart_nir_1",
-#         "nbart_swir_2",
-#         "NDWI",
-#     ]
-#     for var in expected_vars:
-#         assert var in ds.variables
+def test_cols(tb):
+    gdf = tb.ref("poly_gdf")
+    expected_cols = ["attribute",
+                     "area",
+                     "geometry",
+                     "color",
+                     "start_time",
+                     "end_time"]
+    for col in expected_cols:
+        assert col in gdf.columns
+
+
+def test_vars(tb):
+    ds = tb.ref("ds")
+    expected_vars = [
+        "time",
+        "y",
+        "x",
+        "spatial_ref",
+        "nbart_red",
+        "nbart_green",
+        "nbart_blue",
+        "nbart_nir_1",
+        "nbart_swir_2",
+        "NDWI",
+    ]
+    for var in expected_vars:
+        assert var in ds.variables
--- Tests/Frequently_used_code/Contour_extraction_test.py
+++ Tests/Frequently_used_code/Contour_extraction_test.py
@@ -1,40 +1,40 @@
-# import os
-# import pytest
-# from pathlib import Path
-# from testbook import testbook
+import os
+import pytest
+from pathlib import Path
+from testbook import testbook
 
-# TEST_DIR = Path(__file__).parent.parent.resolve()
-# NB_DIR = TEST_DIR.parent
-# NB_PATH = NB_DIR / "Frequently_used_code" / "Contour_extraction.ipynb"
+TEST_DIR = Path(__file__).parent.parent.resolve()
+NB_DIR = TEST_DIR.parent
+NB_PATH = NB_DIR / "Frequently_used_code" / "Contour_extraction.ipynb"
 
 
-# @pytest.fixture(scope="module")
-# def tb():
+@pytest.fixture(scope="module")
+def tb():
     
-#     # Update working directory to ensure relative links in notebooks work
-#     os.chdir(NB_DIR.parent)
+    # Update working directory to ensure relative links in notebooks work
+    os.chdir(NB_DIR.parent)
     
-#     with testbook(NB_PATH, execute=True) as tb:
-#         yield tb
+    with testbook(NB_PATH, execute=True) as tb:
+        yield tb
 
 
-# def test_ok(tb):
-#     assert True  # ok
+def test_ok(tb):
+    assert True  # ok
 
 
-# def test_z_values(tb):
-#     gdf = tb.ref("contours_gdf")
-#     assert "z_value" in gdf.columns
-#     assert gdf.z_value.to_list() == ["550", "600", "650"]
+def test_z_values(tb):
+    gdf = tb.ref("contours_gdf")
+    assert "z_value" in gdf.columns
+    assert gdf.z_value.to_list() == ["550", "600", "650"]
 
 
-# def test_location(tb):
-#     gdf = tb.ref("contours_gdf")
-#     assert "location" in gdf.columns
-#     assert gdf.location.to_list() == ["ACT", "ACT", "ACT"]
+def test_location(tb):
+    gdf = tb.ref("contours_gdf")
+    assert "location" in gdf.columns
+    assert gdf.location.to_list() == ["ACT", "ACT", "ACT"]
 
 
-# def test_time(tb):
-#     gdf = tb.ref("contours_s2_gdf")
-#     assert "time" in gdf.columns
-#     assert gdf.time.to_list() == ["2018-01-05", "2018-02-24"]
+def test_time(tb):
+    gdf = tb.ref("contours_s2_gdf")
+    assert "time" in gdf.columns
+    assert gdf.time.to_list() == ["2018-01-05", "2018-02-24"]
