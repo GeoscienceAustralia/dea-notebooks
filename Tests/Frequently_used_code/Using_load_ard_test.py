460add40a9c0e18d3fc0469b9a486bfbe9a7242b
robbibt <Robbi.BishopTaylor@ga.gov.au>
2022-12-13 06:24:08 +0000

    Specifically test s2cloudless band

--- Tests/Frequently_used_code/Using_load_ard_test.py
+++ Tests/Frequently_used_code/Using_load_ard_test.py
@@ -4,10 +4,10 @@ from testbook import testbook
 
 TEST_DIR = Path(__file__).parent.parent.resolve()
 NB_DIR = TEST_DIR.parent
-NB_PATH = NB_DIR / 'Frequently_used_code' / 'Using_load_ard.ipynb'
+NB_PATH = NB_DIR / "Frequently_used_code" / "Using_load_ard.ipynb"
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def tb():
     with testbook(NB_PATH, execute=True, timeout=180) as tb:
         yield tb
@@ -20,33 +20,22 @@ def test_ok(tb):
 def test_vars(tb):
     ds = tb.ref("ds")
     expected_vars = [
-         'time',
-         'y',
-         'x',
-         'spatial_ref',
-         'nbart_red',
-         'nbart_green',
-         'nbart_blue']
+        "time",
+        "y",
+        "x",
+        "spatial_ref",
+        "nbart_red",
+        "nbart_green",
+        "nbart_blue",
+    ]
     for var in expected_vars:
         assert var in ds.variables
 
 
-def test_s2_hasdata(tb):
+def test_vars_s2(tb):
     ds_s2 = tb.ref("ds_s2")
-
-    assert len(ds_s2.time) > 0
-
-        
-# def test_vars_s2(tb):
-#     ds_s2 = tb.ref("ds_s2")
-#     expected_vars = [
-#          'time',
-#          'y',
-#          'x',
-#          'spatial_ref',
-#          's2cloudless_prob']
-#     for var in expected_vars:
-#         assert var in ds_s2.variables
+    
+    assert "s2cloudless_prob" in ds_s2.variables
 
 
 # def test_vars_dask(tb):
@@ -63,15 +52,16 @@ def test_s2_hasdata(tb):
 #         assert var in ds_dask.variables
 
 
-def is_dask(tb):    
+def is_dask(tb):
     ds_dask = tb.ref("ds_dask")
-    
+
     import dask
+
     assert dask.is_dask_collection(ds_dask)
 
 
-def is_filtered(tb):    
+def is_filtered(tb):
     ds_s2 = tb.ref("ds_s2")
     ds_filtered = tb.ref("ds_filtered")
 
-    assert len(ds_s2.time) > len(ds_filtered.time) 
+    assert len(ds_s2.time) > len(ds_filtered.time)
