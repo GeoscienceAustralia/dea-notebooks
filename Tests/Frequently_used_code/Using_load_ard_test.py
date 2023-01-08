5188c3fdf1a23a0f25c56a9715a9a9b91dc65ec8
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2023-01-09 10:56:12 +1100

    Remove unused tests

--- Tests/Frequently_used_code/Using_load_ard_test.py
+++ Tests/Frequently_used_code/Using_load_ard_test.py
@@ -32,26 +32,6 @@ def test_vars(tb):
         assert var in ds.variables
 
 
-# def test_vars_s2(tb):
-#     ds_s2 = tb.ref("ds_s2")
-    
-#     assert "s2cloudless_prob" in ds_s2.variables
-
-
-# def test_vars_dask(tb):
-#     ds_dask = tb.ref("ds_dask")
-#     expected_vars = [
-#          'time',
-#          'y',
-#          'x',
-#          'spatial_ref',
-#          'nbart_red',
-#          'nbart_green',
-#          'nbart_blue']
-#     for var in expected_vars:
-#         assert var in ds_dask.variables
-
-
 def is_dask(tb):
     ds_dask = tb.ref("ds_dask")
 
