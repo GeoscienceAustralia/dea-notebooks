8f23ca282007008d0e0aa199f4955391c55a6b03
Emma Ai <emma.ai@ga.gov.au>
2023-03-02 04:29:09 +0000

    remove develop installation of dea-tools

--- Tests/test_notebooks.sh
+++ Tests/test_notebooks.sh
@@ -2,7 +2,7 @@
 set -ex
 set -o pipefail
 
-pip3 install -e ./dea-notebooks/Tools
+pip3 install ./dea-notebooks/Tools
 cd ./dea-notebooks
 
 pytest --durations=10 --nbval-lax Beginners_guide DEA_datasets Frequently_used_code/Contour_extraction.ipynb Frequently_used_code/Calculating_band_indices.ipynb Frequently_used_code/Downloading_data_with_STAC.ipynb Frequently_used_code/Exporting_GeoTIFFs.ipynb Frequently_used_code/Generating_composites.ipynb Frequently_used_code/Image_segmentation.ipynb Frequently_used_code/Opening_GeoTIFFs_NetCDFs.ipynb Frequently_used_code/Pansharpening.ipynb Frequently_used_code/Polygon_drill.ipynb Frequently_used_code/Principal_component_analysis.ipynb Frequently_used_code/Rasterize_vectorize.ipynb Frequently_used_code/Using_load_ard.ipynb Frequently_used_code/Virtual_products.ipynb
