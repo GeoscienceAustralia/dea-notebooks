7612ec068ff86b1bb4881d52533cd4dd3a526848
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2023-12-04 17:22:57 +1100

    Add real world examples

--- Tests/test_notebooks.sh
+++ Tests/test_notebooks.sh
@@ -11,4 +11,4 @@ pip3 install ./Tools
 pytest Tests/dea_tools
 
 # Test Juputer Notebooks
-pytest --durations=50 --nbval-lax Beginners_guide DEA_products How_to_guides --ignore How_to_guides/Land_cover_pixel_drill.ipynb --ignore How_to_guides/External_data_ERA5_Climate.ipynb --ignore How_to_guides/Imagery_on_web_map.ipynb
+pytest --durations=50 --nbval-lax Beginners_guide DEA_products How_to_guides Real_world_examples --ignore How_to_guides/Land_cover_pixel_drill.ipynb --ignore How_to_guides/External_data_ERA5_Climate.ipynb --ignore How_to_guides/Imagery_on_web_map.ipynb --ignore Real_world_examples/Mapping_inundation_using_stream_gauges.ipynb --ignore Real_world_examples/Estimate_climate_driver_influence_on_rainfall.ipynb
