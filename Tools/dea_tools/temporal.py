e4bd2cb06124220d76fd324ebd4f259b6f371bc2
robbibt <Robbi.BishopTaylor@ga.gov.au>
2024-05-10 01:22:07 +0000

    Add dask support for lazy computation

--- Tools/dea_tools/temporal.py
+++ Tools/dea_tools/temporal.py
@@ -1,5 +1,5 @@
 ## dea_temporal.py
-'''
+"""
 Conducting temporal (time-domain) analyses on Digital Earth Australia.
 
 License: The code in this notebook is licensed under the Apache License,
@@ -17,15 +17,16 @@ If you would like to report an issue with this script, file one on
 GitHub: https://github.com/GeoscienceAustralia/dea-notebooks/issues/new
     
 Last modified: May 2024
-'''
+"""
 
 import sys
 import dask
+import dask.array as da
+import hdstats
 import warnings
 import numpy as np
 import xarray as xr
 import pandas as pd
-import hdstats
 import scipy.signal
 from scipy.signal import wiener
 from scipy.stats import t
@@ -189,7 +190,7 @@ def _los(da, eos, sos):
     LOS = Length of season (in DOY)
     """
     los = eos - sos
-    #handle negative values
+    # handle negative values
     los = xr.where(
         los >= 0,
         los,
@@ -230,7 +231,7 @@ def xr_phenology(
     ],
     method_sos="first",
     method_eos="last",
-    verbose=True
+    verbose=True,
 ):
     """
     Obtain land surface phenology metrics from an
@@ -412,11 +413,11 @@ def temporal_statistics(da, stats):
         List of temporal statistics to calculate. Options include:
 
         * ``'discordance'``: TODO
-        * ``'f_std'``: std of discrete fourier transform coefficients, returns 
+        * ``'f_std'``: std of discrete fourier transform coefficients, returns
         three layers: f_std_n1, f_std_n2, f_std_n3
-        * ``'f_mean'``: mean of discrete fourier transform coefficients, returns 
+        * ``'f_mean'``: mean of discrete fourier transform coefficients, returns
         three layers: f_mean_n1, f_mean_n2, f_mean_n3
-        * ``'f_median'``: median of discrete fourier transform coefficients, returns 
+        * ``'f_median'``: median of discrete fourier transform coefficients, returns
         three layers: f_median_n1, f_median_n2, f_median_n3
         * ``'mean_change'``: mean of discrete difference along time dimension
         * ``'median_change'``: median of discrete difference along time dimension
@@ -582,10 +583,9 @@ def temporal_statistics(da, stats):
     return ds
 
 
-def time_buffer(input_date, buffer='30 days', output_format='%Y-%m-%d'):
-
+def time_buffer(input_date, buffer="30 days", output_format="%Y-%m-%d"):
     """
-    Create a buffer of a given duration (e.g. days) around a time query. 
+    Create a buffer of a given duration (e.g. days) around a time query.
     Output is a string in the correct format for a datacube query.
 
     Parameters
@@ -593,36 +593,40 @@ def time_buffer(input_date, buffer='30 days', output_format='%Y-%m-%d'):
     input_date : str, yyyy-mm-dd
         Time to buffer
     buffer : str, optional
-        Default is '30 days', can be any string supported by the 
-        `pandas.Timedelta` function 
+        Default is '30 days', can be any string supported by the
+        `pandas.Timedelta` function
     output_format : str, optional
         Optional string giving the `strftime` format used to convert
-        buffered times to strings; defaults to '%Y-%m-%d' 
+        buffered times to strings; defaults to '%Y-%m-%d'
         (e.g. '2017-12-02')
-            
+
     Returns
     -------
     early_buffer, late_buffer : str
         A tuple of strings to pass to the datacube query function
-        e.g. `('2017-12-02', '2018-01-31')` for input 
-        `input_date='2018-01-01'` and `buffer='30 days'`  
+        e.g. `('2017-12-02', '2018-01-31')` for input
+        `input_date='2018-01-01'` and `buffer='30 days'`
     """
     # Use assertions to check we have the correct function input
-    assert isinstance(input_date, str), "Input date must be a string in quotes in 'yyyy-mm-dd' format"
-    assert isinstance(buffer, str), "Buffer must be a string supported by `pandas.Timedelta`, e.g. '5 days'"
-    
+    assert isinstance(
+        input_date, str
+    ), "Input date must be a string in quotes in 'yyyy-mm-dd' format"
+    assert isinstance(
+        buffer, str
+    ), "Buffer must be a string supported by `pandas.Timedelta`, e.g. '5 days'"
+
     # Convert inputs to pandas format
     buffer = pd.Timedelta(buffer)
     input_date = pd.to_datetime(input_date)
-    
+
     # Apply buffer
     early_buffer = input_date - buffer
     late_buffer = input_date + buffer
-    
+
     # Convert back to string using strftime
     early_buffer = early_buffer.strftime(output_format)
     late_buffer = late_buffer.strftime(output_format)
-    
+
     return early_buffer, late_buffer
 
 
@@ -636,7 +640,7 @@ def calculate_vector_stat(
     window="hann",
 ):
     """Calculates a vector statistic over a rolling window.
-    
+
     Parameters
     ----------
     vec : d-dimensional np.ndarray
@@ -656,7 +660,7 @@ def calculate_vector_stat(
         also want to use 'boxcar'. Any scipy window
         function is allowed (see documentation for scipy.signal.get_window
         for more information).
-        
+
     Returns
     -------
     (d / step)-dimensional np.ndarray
@@ -698,11 +702,14 @@ class LinregressResult:
         self.intercept = intercept
         self.pval = pval
         self.stderr = stderr
-    
+
     def __repr__(self):
-        return 'LinregressResult({})'.format(
-            ', '.join('{}={}'.format(k, getattr(self, k))
-                      for k in dir(self) if not k.startswith('_'))
+        return "LinregressResult({})".format(
+            ", ".join(
+                "{}={}".format(k, getattr(self, k))
+                for k in dir(self)
+                if not k.startswith("_")
+            )
         )
 
 
@@ -775,12 +782,12 @@ def lag_linregress_3D(x, y, lagx=0, lagy=0, first_dim="time"):
     cor = cov / (xstd * ystd)
 
     # 6. Compute regression slope and intercept:
-    slope = cov / (xstd ** 2)
+    slope = cov / (xstd**2)
     intercept = ymean - xmean * slope
 
     # 7. Compute P-value and standard error
     # Compute t-statistics
-    tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor ** 2)
+    tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor**2)
     stderr = slope / tstats
 
     from scipy.stats import t
@@ -832,9 +839,23 @@ def xr_regression(x, y, lag_x=0, lag_y=0, dim="time", alternative="two-sided"):
         A dataset comparing the two input datasets along their aligned
         dimension, containing variables including covariance, correlation,
         coefficient of determination, regression slope, intercept,
-        p-value and standard error, and number of valid observations (n). 
+        p-value and standard error, and number of valid observations (n).
 
     """
+
+    def _pvalue(tstats, n, alternative):
+        """
+        Function for lazily calculating p-values.
+        (without this, dask computation occurs too early)
+        """
+        if alternative == "two-sided":
+            pval = t.sf(np.abs(tstats), n - 2) * 2
+        elif alternative == "greater":
+            pval = t.sf(tstats, n - 2)
+        elif alternative == "less":
+            pval = t.cdf(np.abs(tstats), n - 2)
+
+        return pval
 
     # Shift x and y data if lags are specified
     if lag_x != 0:
@@ -870,19 +891,34 @@ def xr_regression(x, y, lag_x=0, lag_y=0, dim="time", alternative="two-sided"):
     intercept = ymean - xmean * slope
 
     # Compute t-statistics and standard error
-    tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor**2)
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore")
+        tstats = cor * np.sqrt(n - 2) / np.sqrt(1 - cor**2)
     stderr = slope / tstats
 
-    # Calculate p-values for different alternative hypotheses
-    if alternative == "two-sided":
-        pval = t.sf(np.abs(tstats), n - 2) * 2
-    elif alternative == "greater":
-        pval = t.sf(tstats, n - 2)
-    elif alternative == "less":
-        pval = t.cdf(np.abs(tstats), n - 2)
+    # Calculate p-values for different alternative hypotheses.
+    # If data is dask, then delay computation of p-value
+    if dask.is_dask_collection(cor):
 
-    # Wrap output as xarray and combine into single dataset
-    pval = xr.DataArray(pval, dims=cor.dims, coords=cor.coords)
+        _pvalue_lazy = dask.delayed(_pvalue)
+        pval = xr.DataArray(
+            da.from_delayed(
+                _pvalue_lazy(tstats, n, alternative),
+                shape=cor.shape,
+                dtype=cor.dtype,
+            ),
+            dims=cor.dims,
+            coords=cor.coords,
+        ).chunk(cor.chunksizes)
+    
+    else:
+        pval = xr.DataArray(
+            _pvalue(tstats, n, alternative),
+            dims=cor.dims,
+            coords=cor.coords,
+        )
+
+    # Combine into single dataset
     regression_ds = xr.merge(
         [
             cov.rename("cov"),
@@ -901,12 +937,12 @@ def xr_regression(x, y, lag_x=0, lag_y=0, dim="time", alternative="two-sided"):
 
 def calculate_sad(vec):
     """Calculates the surface area duration curve for a given vector of heights.
-    
+
     Parameters
     ----------
     vec : d-dimensional np.ndarray
         Vector of heights over time.
-    
+
     Returns
     -------
     d-dimensional np.ndarray
@@ -917,7 +953,7 @@ def calculate_sad(vec):
 
 def calculate_stsad(vec, window_size=365, step=10, progress=None, window="hann"):
     """Calculates the short-time surface area duration curve for a given vector of heights.
-    
+
     Parameters
     ----------
     vec : d-dimensional np.ndarray
@@ -933,7 +969,7 @@ def calculate_stsad(vec, window_size=365, step=10, progress=None, window="hann")
         also want to use 'boxcar'. Any scipy window
         function is allowed (see documentation for scipy.signal.get_window
         for more information).
-    
+
     Returns
     -------
     (d / step)-dimensional np.ndarray
@@ -952,4 +988,3 @@ def calculate_stsad(vec, window_size=365, step=10, progress=None, window="hann")
         progress=progress,
         window=window,
     )
-
