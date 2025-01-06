9074bc69719b6fd43e951409a34b0b1873d56d88
LaurenSchenk1 <lauren.schenk@outlook.com>
2025-01-06 04:27:50 +0000

    debugging wetlands app, not displaying status

--- Tools/dea_tools/wit.py
+++ Tools/dea_tools/wit.py
@@ -60,6 +60,7 @@ def WIT_drill(
         time=time,
         group_by="solar_day",
         dask_chunks={"time":1, "x": 2048, "y": 2048},
+        verbose=verbose,
     )
     
     # Load into memory using Dask
