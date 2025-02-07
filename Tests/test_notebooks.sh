df14cee3bd5a2af2c3148b773a63cd3e7f170b63
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2025-02-07 18:27:00 +1100

    Test custom params

--- Tests/test_notebooks.sh
+++ Tests/test_notebooks.sh
@@ -7,12 +7,22 @@ set -o pipefail
 cd ./dea-notebooks
 pip3 install ./Tools
 
-# Test DEA Tools functions
-pytest Tests/dea_tools
-
-# Test Juputer Notebooks
-pytest --durations=100 --nbval-lax Beginners_guide
-
-# pytest --durations=100 --nbval-lax Beginners_guide DEA_products How_to_guides Real_world_examples --ignore How_to_guides/Land_cover_pixel_drill.ipynb --ignore How_to_guides/External_data_ERA5_Climate.ipynb --ignore How_to_guides/Imagery_on_web_map.ipynb --ignore Real_world_examples/Mapping_inundation_using_stream_gauges.ipynb --ignore Real_world_examples/Estimate_climate_driver_influence_on_rainfall.ipynb
-# pytest --durations=100 --nbval-lax Real_world_examples --ignore How_to_guides/Land_cover_pixel_drill.ipynb --ignore How_to_guides/External_data_ERA5_Climate.ipynb --ignore How_to_guides/Imagery_on_web_map.ipynb --ignore Real_world_examples/Mapping_inundation_using_stream_gauges.ipynb --ignore Real_world_examples/Estimate_climate_driver_influence_on_rainfall.ipynb
-# pytest --durations=100 --nbval-lax Beginners_guide DEA_products How_to_guides Real_world_examples --ignore How_to_guides/Land_cover_pixel_drill.ipynb --ignore How_to_guides/External_data_ERA5_Climate.ipynb --ignore How_to_guides/Imagery_on_web_map.ipynb --ignore Real_world_examples/Mapping_inundation_using_stream_gauges.ipynb --ignore Real_world_examples/Estimate_climate_driver_influence_on_rainfall.ipynb
+if [ "$#" -eq 0 ]; then
+    echo "No parameter provided. Running all tests..."
+    pytest --durations=10 --nbval-lax Beginners_guide DEA_products --ignore DEA_products/DEA_Wetlands_Insight_Tool.ipynb How_to_guides/Animated_timeseries.ipynb How_to_guides/Contour_extraction.ipynb How_to_guides/Calculating_band_indices.ipynb How_to_guides/Exporting_GeoTIFFs.ipynb How_to_guides/Detecting_seasonality.ipynb How_to_guides/Generating_composites.ipynb How_to_guides/Image_segmentation.ipynb How_to_guides/Interpolation.ipynb How_to_guides/Opening_GeoTIFFs_NetCDFs.ipynb How_to_guides/Pansharpening.ipynb How_to_guides/Planetary_computer.ipynb How_to_guides/Polygon_drill.ipynb How_to_guides/Principal_component_analysis.ipynb How_to_guides/Rasterize_vectorize.ipynb How_to_guides/Tidal_modelling.ipynb How_to_guides/Using_load_ard.ipynb How_to_guides/Virtual_products.ipynb Real_world_examples/Coastal_erosion.ipynb Real_world_examples/Intertidal_elevation.ipynb
+elif [ "$1" = "dea_tools" ]; then
+    echo "Testing DEA Tools..."
+    pytest Tests/dea_tools
+elif [ "$1" = "beginners_guide" ]; then
+    echo "Testing Beginners_guide..."
+    pytest --durations=100 --nbval-lax Beginners_guide
+elif [ "$1" = "dea_products" ]; then
+    echo "Testing DEA_products..."
+    pytest --durations=100 --nbval-lax DEA_products
+elif [ "$1" = "how_to_guides" ]; then
+    echo "Testing How_to_guides..."
+    pytest --durations=100 --nbval-lax How_to_guides --ignore How_to_guides/Land_cover_pixel_drill.ipynb --ignore How_to_guides/External_data_ERA5_Climate.ipynb --ignore How_to_guides/Imagery_on_web_map.ipynb
+else
+    echo "Unknown parameter: $1"
+    ls -l
+fi
