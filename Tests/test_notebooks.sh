cdaf691a12183523b2b072d0373b1d69e89e079b
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2025-02-07 18:30:15 +1100

    Test passing empty value

--- Tests/test_notebooks.sh
+++ Tests/test_notebooks.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# pipe the exit code to parent process
+# Pipe the exit code to parent process
 set -ex
 set -o pipefail
 
 cd ./dea-notebooks
 pip3 install ./Tools
 
+# Run requested tests
 if [ "$#" -eq 0 ]; then
     echo "No parameter provided. Running all tests..."
     pytest --durations=10 --nbval-lax Beginners_guide DEA_products --ignore DEA_products/DEA_Wetlands_Insight_Tool.ipynb How_to_guides/Animated_timeseries.ipynb How_to_guides/Contour_extraction.ipynb How_to_guides/Calculating_band_indices.ipynb How_to_guides/Exporting_GeoTIFFs.ipynb How_to_guides/Detecting_seasonality.ipynb How_to_guides/Generating_composites.ipynb How_to_guides/Image_segmentation.ipynb How_to_guides/Interpolation.ipynb How_to_guides/Opening_GeoTIFFs_NetCDFs.ipynb How_to_guides/Pansharpening.ipynb How_to_guides/Planetary_computer.ipynb How_to_guides/Polygon_drill.ipynb How_to_guides/Principal_component_analysis.ipynb How_to_guides/Rasterize_vectorize.ipynb How_to_guides/Tidal_modelling.ipynb How_to_guides/Using_load_ard.ipynb How_to_guides/Virtual_products.ipynb Real_world_examples/Coastal_erosion.ipynb Real_world_examples/Intertidal_elevation.ipynb
@@ -22,6 +23,9 @@ elif [ "$1" = "dea_products" ]; then
 elif [ "$1" = "how_to_guides" ]; then
     echo "Testing How_to_guides..."
     pytest --durations=100 --nbval-lax How_to_guides --ignore How_to_guides/Land_cover_pixel_drill.ipynb --ignore How_to_guides/External_data_ERA5_Climate.ipynb --ignore How_to_guides/Imagery_on_web_map.ipynb
+elif [ "$1" = "real_world_examples" ]; then
+    echo "Testing Real_world_examples..."
+    pytest --durations=10 --nbval-lax Real_world_examples/Coastal_erosion.ipynb Real_world_examples/Intertidal_elevation.ipynb
 else
     echo "Unknown parameter: $1"
     ls -l
