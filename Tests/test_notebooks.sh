1de71de937db0044c827fb6e849307d3b9531e9d
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2025-02-10 09:47:53 +1100

    Update tests to add modified check

--- Tests/test_notebooks.sh
+++ Tests/test_notebooks.sh
@@ -18,27 +18,26 @@ if [ -z "$1" ]; then
     echo "No parameter provided. Running default tests..."
     
     # Run pytest on default subset of notebooks, ignoring specific ones
-    pytest --durations=10 --nbval-lax \
+    pytest --durations=20 --nbval-lax \
         Beginners_guide \
-        DEA_products \
-        --ignore DEA_products/DEA_Wetlands_Insight_Tool.ipynb \
-        --ignore How_to_guides/Animated_timeseries.ipynb \
-        --ignore How_to_guides/Contour_extraction.ipynb \
-        --ignore How_to_guides/Calculating_band_indices.ipynb \
-        --ignore How_to_guides/Exporting_GeoTIFFs.ipynb \
-        --ignore How_to_guides/Detecting_seasonality.ipynb \
-        --ignore How_to_guides/Generating_composites.ipynb \
-        --ignore How_to_guides/Image_segmentation.ipynb \
-        --ignore How_to_guides/Interpolation.ipynb \
-        --ignore How_to_guides/Opening_GeoTIFFs_NetCDFs.ipynb \
-        --ignore How_to_guides/Pansharpening.ipynb \
-        --ignore How_to_guides/Planetary_computer.ipynb \
-        --ignore How_to_guides/Polygon_drill.ipynb \
-        --ignore How_to_guides/Principal_component_analysis.ipynb \
-        --ignore How_to_guides/Rasterize_vectorize.ipynb \
-        --ignore How_to_guides/Tidal_modelling.ipynb \
-        --ignore How_to_guides/Using_load_ard.ipynb \
-        --ignore How_to_guides/Virtual_products.ipynb \
+        DEA_products --ignore DEA_products/DEA_Wetlands_Insight_Tool.ipynb \
+        How_to_guides/Animated_timeseries.ipynb \
+        How_to_guides/Contour_extraction.ipynb \
+        How_to_guides/Calculating_band_indices.ipynb \
+        How_to_guides/Exporting_GeoTIFFs.ipynb \
+        How_to_guides/Detecting_seasonality.ipynb \
+        How_to_guides/Generating_composites.ipynb \
+        How_to_guides/Image_segmentation.ipynb \
+        How_to_guides/Interpolation.ipynb \
+        How_to_guides/Opening_GeoTIFFs_NetCDFs.ipynb \
+        How_to_guides/Pansharpening.ipynb \
+        How_to_guides/Planetary_computer.ipynb \
+        How_to_guides/Polygon_drill.ipynb \
+        How_to_guides/Principal_component_analysis.ipynb \
+        How_to_guides/Rasterize_vectorize.ipynb \
+        How_to_guides/Tidal_modelling.ipynb \
+        How_to_guides/Using_load_ard.ipynb \
+        How_to_guides/Virtual_products.ipynb \
         Real_world_examples/Coastal_erosion.ipynb \
         Real_world_examples/Intertidal_elevation.ipynb
 
@@ -48,22 +47,22 @@ elif [ "$1" = "dea_tools" ]; then
 
 elif [ "$1" = "beginners_guide" ]; then
     echo "Testing Beginners_guide..."
-    pytest --durations=100 --nbval-lax Beginners_guide
+    pytest --durations=20 --nbval-lax Beginners_guide
 
 elif [ "$1" = "dea_products" ]; then
     echo "Testing DEA_products..."
-    pytest --durations=100 --nbval-lax DEA_products
+    pytest --durations=20 --nbval-lax DEA_products
 
 elif [ "$1" = "how_to_guides" ]; then
     echo "Testing How_to_guides..."
-    pytest --durations=100 --nbval-lax How_to_guides \
+    pytest --durations=20 --nbval-lax How_to_guides \
         --ignore How_to_guides/Land_cover_pixel_drill.ipynb \
         --ignore How_to_guides/External_data_ERA5_Climate.ipynb \
         --ignore How_to_guides/Imagery_on_web_map.ipynb
 
 elif [ "$1" = "real_world_examples" ]; then
     echo "Testing Real_world_examples..."
-    pytest --durations=10 --nbval-lax Real_world_examples \
+    pytest --durations=20 --nbval-lax Real_world_examples \
         --ignore Real_world_examples/Estimate_climate_driver_influence_on_rainfall.ipynb \
         --ignore Real_world_examples/Mapping_inundation_using_stream_gauges.ipynb
 
