556376b6cf4ae6fc77ca29919433b3fe271be708
Jenna Guffogg <jenna.guffogg@gmail.com>
2025-05-27 11:22:17 +1000

    added ls7 coeffs

--- Tools/dea_tools/bandindices.py
+++ Tools/dea_tools/bandindices.py
@@ -261,6 +261,16 @@ def calculate_indices(ds,
                   'TCW_ls8ls9': lambda ds: (0.1511 * ds.blue + 0.1973 * ds.green +
                                      0.3283 * ds.red + 0.3407 * ds.nir +
                                     -0.7117 * ds.swir1 + -0.4559 * ds.swir2),
+                  
+                  # Tasseled Cap Wetness for Landsat 7
+                  'TCW_ls7': lambda ds: (0.2626 * ds.blue + 0.2141 * ds.green +
+                                     0.0926 * ds.red + 0.0656 * ds.nir +
+                                    -0.7629 * ds.swir1 + -0.5388 * ds.swir2),
+                  
+                  # note the TC coefficients from Crist appear to be for LS5
+                  'TCW_ls5': lambda ds: (0.0315 * ds.blue + 0.2021 * ds.green +
+                                     0.3102 * ds.red + 0.1594 * ds.nir +
+                                    -0.6806 * ds.swir1 + -0.6109 * ds.swir2),
 
 
                   # Tasseled Cap Greeness, Crist 1985
@@ -268,9 +278,19 @@ def calculate_indices(ds,
                                      -0.4934 * ds.red + 0.7940 * ds.nir +
                                      -0.0002 * ds.swir1 + -0.1446 * ds.swir2),
                   
+                  # Tasseled Cap Greeness for Landsat 8 and 9
                   'TCG_ls8ls9': lambda ds: (-0.2941 * ds.blue + -0.2430 * ds.green +
                                      -0.5424 * ds.red + 0.7276 * ds.nir +
                                      0.0713 * ds.swir1 + -0.1608 * ds.swir2),
+                  
+                  # Tasseled Cap Greeness for Landsat 7
+                  'TCG_ls7': lambda ds: (-0.3344 * ds.blue + -0.3544 * ds.green +
+                                     -0.4556 * ds.red + 0.6966 * ds.nir +
+                                     -0.0242 * ds.swir1 + -0.2630 * ds.swir2),
+                  
+                  'TCG_ls5': lambda ds: (-0.1603 * ds.blue + -0.2819 * ds.green +
+                                     -0.4934 * ds.red + 0.7940 * ds.nir +
+                                     -0.0002 * ds.swir1 + -0.1446 * ds.swir2),
                   
 
                   # Tasseled Cap Brightness, Crist 1985
@@ -278,9 +298,19 @@ def calculate_indices(ds,
                                      0.5524 * ds.red + 0.5741 * ds.nir +
                                      0.3124 * ds.swir1 + -0.2303 * ds.swir2),
                   
+                  # Tasseled Cap Brightness for Landsat 8 and 9
                   'TCB_ls8ls9': lambda ds: (0.3029 * ds.blue + 0.2786 * ds.green +
                                      0.4733 * ds.red + 0.5599 * ds.nir +
                                      0.5080 * ds.swir1 + 0.1872 * ds.swir2),
+                  
+                  # Tasseled Cap Brightness for Landsat 7
+                  'TCB_ls7': lambda ds: (0.3561 * ds.blue + 0.3972 * ds.green +
+                                     0.3904 * ds.red + 0.6966 * ds.nir +
+                                     0.2286 * ds.swir1 + 0.1596 * ds.swir2),
+                  
+                  'TCB_ls5': lambda ds: (0.2043 * ds.blue + 0.4158 * ds.green +
+                                     0.5524 * ds.red + 0.5741 * ds.nir +
+                                     0.3124 * ds.swir1 + -0.2303 * ds.swir2),
                   
                   # Tasseled Cap Transformations with Sentinel-2 coefficients 
                   # after Nedkov 2017 using Gram-Schmidt orthogonalization (GSO)
