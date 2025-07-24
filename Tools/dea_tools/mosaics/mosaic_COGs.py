b739e03cebad22dc097f5465040c23871f369b0e
Abeer <abeer.mahendroo@ga.gov.au>
2025-07-25 00:55:12 +1000

    Update mosaic_COGs.py

--- Tools/dea_tools/mosaics/mosaic_COGs.py
+++ Tools/dea_tools/mosaics/mosaic_COGs.py
@@ -291,6 +291,8 @@ def make_cog_mosaics(
             log.info(f"{run_id}: Building virtual raster (VRT)")
             os.environ['AWS_NO_SIGN_REQUEST'] = 'YES'
             os.environ['GDAL_DISABLE_READDIR_ON_OPEN']='YES'
+            os.environ['CPL_CURL_VERBOSE']='YES'
+            CPL_CURL_VERBOSE
             try:
                 subprocess.run(
                     ["gdalbuildvrt", vrt_name, "-input_file_list", file_list_name],
