50147b0169e2b269561660f8b850285298af6920
Abeer <abeer.mahendroo@ga.gov.au>
2025-07-25 17:59:10 +1000

    Update mosaic_COGs.py

--- Tools/dea_tools/mosaics/mosaic_COGs.py
+++ Tools/dea_tools/mosaics/mosaic_COGs.py
@@ -115,7 +115,7 @@ def _get_vsicurlhttp_from_s3(s3_url):
             "dea-public-data-dev/", "/vsicurl/https://dea-public-data-dev.s3-ap-southeast-2.amazonaws.com/"
         )
     if "dea-public-data/" in s3_url:
-        return s3_url.replace("dea-public-data/", "/vsicurl/https://data.dea.ga.gov.au/")
+        return s3_url.replace("dea-public-data/", "/vsicurl/https://https://dea-public-data.s3-ap-southeast-2.amazonaws.com/")
     raise ValueError(f"Unexpected S3 URL structure: {s3_url}")
 
 
