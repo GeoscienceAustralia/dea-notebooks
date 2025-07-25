1238aae3a48f8bd9cebe86aa0428654f343ca309
cbur24 <c.a.burton123@gmail.com>
2025-07-25 08:18:22 +0000

    more efficient algo working

--- Tools/dea_tools/validation.py
+++ Tools/dea_tools/validation.py
@@ -1,4 +1,4 @@
-## validation.py
+# validation.py
 """
 Tools for validating outputs and producing accuracy assessment metrics.
 
@@ -27,6 +27,7 @@ from math import sqrt
 from scipy import stats
 import geopandas as gpd
 from odc.geo.xr import assign_crs
+from shapely.geometry import Point
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 
@@ -81,26 +82,29 @@ def eval_metrics(x, y, round=3, all_regress=False):
 
     # Additional regression params
     if all_regress:
-        stats_dict.update({
-            "Regression p-value": lin_reg.pvalue,
-            "Regression intercept": lin_reg.intercept,
-            "Regression standard error": lin_reg.stderr,
-        })
+        stats_dict.update(
+            {
+                "Regression p-value": lin_reg.pvalue,
+                "Regression intercept": lin_reg.intercept,
+                "Regression standard error": lin_reg.stderr,
+            }
+        )
 
     # Return as
     return pd.Series(stats_dict).round(round)
 
-def random_sampling(da,
-                    n=None,
-                    sampling='stratified_random',
-                    manual_class_ratios=None,
-                    out_fname=None
-                   ):
-    
+
+def random_sampling(
+    da, n=None,
+    sampling="stratified_random",
+    manual_class_ratios=None,
+    out_fname=None,
+    oversample_factor=5
+):
     """
-    Creates randomly sampled points for either post-classification
-    accuracy assessment, and training data
-    
+    Efficient and scalable random sampling of a 2D classified xarray.DataArray.
+    Returns a GeoDataFrame of point samples based on specified sampling strategy.
+
     Params:
     -------
     da: xarray.DataArray
@@ -109,16 +113,16 @@ def random_sampling(da,
         Total number of points to sample. Ignored if providing
         a dictionary of {class:numofpoints} to 'manual_class_ratios'
     sampling: str
-        'stratified_random' = Create points that are randomly 
+        'stratified_random' = Create points that are randomly
         distributed within each class, where each class has a
-        number of points proportional to its relative area. 
+        number of points proportional to its relative area.
         'equal_stratified_random' = Create points that are randomly
         distributed within each class, where each class has the
         same number of points.
         'random' = Create points that are randomly distributed
         throughout the image.
-        'manual' = user definined, each class is allocated a 
-        specified number of points, supply a manual_class_ratio 
+        'manual' = user definined, each class is allocated a
+        specified number of points, supply a manual_class_ratio
         dictionary mapping number of points to each class
     manual_class_ratios: dict
         If setting sampling to 'manual', the provide a dictionary
@@ -128,112 +132,150 @@ def random_sampling(da,
         If providing a filepath name, e.g 'sample_points.shp', the
         function will export a shapefile/geojson of the sampling
         points to file.
-    
+
     Output
     ------
     GeoPandas.Dataframe
-    
-    """
-    
-    if sampling not in ['stratified_random', 'equal_stratified_random', 'random', 'manual']:
-        raise ValueError("Sampling strategy must be one of 'stratified_random', "+
-                             "'equal_stratified_random', 'random', or 'manual'") 
-    
-    #open the dataset as a pandas dataframe
-    da = da.squeeze()
-    df = da.to_dataframe(name='class')
-    df = df.dropna(how='any')
-    
-    #list to store points
-    samples = []
-    
-    if sampling == 'stratified_random':
-        #determine class ratios in image
-        class_ratio = pd.DataFrame({'proportion': df['class'].value_counts(normalize=True),
-                                    'class':df['class'].value_counts(normalize=True).keys()
-                                 })
-        
-        for _class in class_ratio['class']:
-            #use relative proportions of classes to sample df
-            no_of_points = n * class_ratio[class_ratio['class']==_class]['proportion'].values[0]
-            #random sample each class
-            print('Class '+ str(_class)+ ': sampling at '+ str(round(no_of_points)) + ' coordinates')
-            sample_loc = df[df['class'] == _class].sample(n=int(round(no_of_points)))
-            samples.append(sample_loc)
 
-    if sampling == 'equal_stratified_random':
-        classes = np.unique(df['class'])
-        
-        for _class in classes:
-            #use relative proportions of classes to sample df
-            no_of_points = n / len(classes)
-            #random sample each classes
-            try:
-                sample_loc = df[df['class'] == _class].sample(n=int(round(no_of_points)))
-                print('Class '+ str(_class)+ ': sampling at '+ str(round(no_of_points)) + ' coordinates')
-                samples.append(sample_loc)
-            
-            except ValueError:
-                        print('Requested more sample points than population of pixels for class '+ str(_class)+', skipping')
-                        pass
-    
-    if sampling == 'random':
-        no_of_points = n
-        #random sample entire df
-        print('Randomly sampling dataAraay at '+ str(round(no_of_points)) + ' coordinates')
-        sample_loc = df.dropna().sample(n=int(round(no_of_points)))
-        samples.append(sample_loc)
-    
-    if sampling == 'manual':
-        if isinstance(manual_class_ratios, dict):
-            #check classes in dict match classes in data
-            classes = np.unique(df['class'])
-            dict_classes = list(manual_class_ratios.keys())
-            
-            if set(dict_classes).issubset([str(i) for i in classes]):
-                #mask for just those classes in the provided dictionary
-                mask = np.isin(classes,
-                               np.array(dict_classes).astype(type(classes[0])))
-                classes = classes[mask]               
-                #run sampling
-                for _class in classes:
-                    no_of_points = manual_class_ratios.get(str(_class))
-                    #random sample each class
-                    try:
-                        sample_loc = df[df['class'] == _class].sample(n=int(round(no_of_points)))
-                        print('Class '+ str(_class)+ ': sampled at '+ str(round(no_of_points)) + ' coordinates')
-                        samples.append(sample_loc)
-                        
-                    except ValueError:
-                        print('Requested more sample points than population of pixels for class '+ str(_class)+', skipping')
-                        pass
+    """
+
+    if sampling not in [
+        "stratified_random",
+        "equal_stratified_random",
+        "random",
+        "manual",
+    ]:
+        raise ValueError(
+            "Sampling strategy must be one of 'stratified_random', 'equal_stratified_random', 'random', or 'manual'"
+        )
+
+    if "time" in da.dims:
+        raise ValueError("Input DataArray must not have a 'time' dimension.")
+
+    if len(da.dims) > 2:
+        raise ValueError(
+            "Input DataArray must not have more than two dimensions")
+
+    if not isinstance(da, xr.DataArray):
+        raise ValueError(
+            "This function only accepts xarray.DataArrays as input")
+
+    # Standardize spatial dims
+    x_names = ["x", "longitude", "lon"]
+    y_names = ["y", "latitude", "lat"]
+    x_dim = next((dim for dim in da.dims if dim.lower() in x_names), None)
+    y_dim = next((dim for dim in da.dims if dim.lower() in y_names), None)
+    if x_dim is None or y_dim is None:
+        raise ValueError(
+            f"Could not infer spatial dimensions. Found dims: {da.dims}")
+    da = da.rename({x_dim: "x", y_dim: "y"})
+
+    #grab data as numpy arrays and count classes
+    data = da.values
+    unique_classes, class_counts = np.unique(
+        data[~np.isnan(data)], return_counts=True)
+    unique_classes = unique_classes.astype(int)
+
+    samples = []
+
+    def unravel_sample(flat_idx):
+        y_idx, x_idx = np.unravel_index(flat_idx, data.shape)
+        return da["y"].values[y_idx], da["x"].values[x_idx]
+
+    if sampling == "random":
+        total_valid = (~np.isnan(data)).sum()
+        if n > total_valid:
+            raise ValueError(
+                "Requested more samples than available valid pixels.")
+        print(f"Sampling {n} points")
+        flat_indices = np.flatnonzero(~np.isnan(data))
+        sampled = np.random.choice(flat_indices, size=n, replace=False)
+        for idx in sampled:
+            y, x = np.unravel_index(idx, data.shape)
+            y_val = da["y"].values[y]
+            x_val = da["x"].values[x]
+            cls = data[y, x]
+            samples.append((y_val, x_val, int(cls)))
+
+    elif sampling in ["stratified_random", "equal_stratified_random", "manual"]:
+        if sampling == "equal_stratified_random":
+            n_per_class = int(np.ceil(n / len(unique_classes)))
+            class_sample_sizes = {cls: n_per_class for cls in unique_classes}
+        elif sampling == "stratified_random":
+            proportions = class_counts / class_counts.sum()
+            class_sample_sizes = {
+                cls: int(np.round(n * prop))
+                for cls, prop in zip(unique_classes, proportions)
+            }
+        elif sampling == "manual":
+            if not isinstance(manual_class_ratios, dict):
+                raise ValueError(
+                    "Must provide manual_class_ratios for manual sampling."
+                )
+            class_sample_sizes = {
+                int(k): int(v) for k, v in manual_class_ratios.items()
+            }
+
+        for cls in class_sample_sizes:
+            sample_size = class_sample_sizes[cls]
+            print(f"Class {cls}: sampling {sample_size} points")
+
+            class_mask = data == cls
+            class_count = class_mask.sum()
+            if (
+                class_count > 1e9
+            ):  # For v. large classes, sample random coords first and check matches
+                # Try oversampling until we get enough
+                n_try = int(sample_size * oversample_factor)
+                rand_x = np.random.choice(
+                    np.arange(len(da.x)), n_try, replace=False)
+                
+                rand_y = np.random.choice(
+                    np.arange(len(da.y)), n_try, replace=False)
+                
+                match = data[rand_y, rand_x] == cls
+                rand_y, rand_x = rand_y[match], rand_x[match]
+                if len(rand_y) < sample_size:
+                    print(f"Warning: insufficient matches for class {cls}, "
+                          f"try increasing oversampling. Returning {len(rand_y)-1} matches")
+
+                    idx = np.random.choice(np.arange(len(rand_y)), size=len(rand_y) - 1, replace=False)
+                    for i in idx:
+                        y = da["y"].values[rand_y[i]]
+                        x = da["x"].values[rand_x[i]]
+                        samples.append((y, x, cls))
+
+                else:
+                    idx = np.random.choice(np.arange(len(rand_y)), size=sample_size, replace=False)
+                    for i in idx:
+                        y = da["y"].values[rand_y[i]]
+                        x = da["x"].values[rand_x[i]]
+                        samples.append((y, x, cls))
 
             else:
-                raise ValueError("Some or all of the classes in 'manual_class_ratio' dictionary do not" +
-                                 " match the classes in the supplied dataArray. "+
-                                "DataArray classes: "+str(classes)+", Supplied dict classes: "+
-                                 str(list(manual_class_ratios.keys())))
-            
-        else:
-            raise ValueError("Must supply a dictionary mapping {'class': numofpoints} if sampling" +
-                             " is set to 'manual'")
-    
-    #join back into single datafame
-    all_samples = pd.concat([samples[i] for i in range(0,len(samples))])
-        
-    #get pd.mulitindex coords as list 
-    y = [i[0] for i in list(all_samples.index)]
-    x = [i[1] for i in list(all_samples.index)]
+                flat_idx = np.argwhere(class_mask).squeeze()
+                if flat_idx.size < sample_size:
+                    print(
+                        f"Warning: not enough pixels in class  {cls} for given sample size, skipping")
+                    continue
+                picked = np.random.choice(
+                    flat_idx.shape[0], sample_size, replace=False)
+                ys, xs = flat_idx[picked].T
+                for y_i, x_i in zip(ys, xs):
+                    y = da["y"].values[y_i]
+                    x = da["x"].values[x_i]
+                    samples.append((y, x, cls))
 
-    #create geopandas dataframe
+    if len(samples) == 0:
+        raise RuntimeError("No samples collected. Check input conditions.")
+
+    df = pd.DataFrame(samples, columns=["y", "x", "class"])
     gdf = gpd.GeoDataFrame(
-        all_samples,
-        crs=f'EPSG:{da.odc.crs.epsg}',
-        geometry=gpd.points_from_xy(x,y)).reset_index()
+        df, geometry=gpd.points_from_xy(df.x, df.y), crs=f"EPSG:{da.odc.crs.epsg}"
+    )
+    gdf = gdf.drop(["x", "y"], axis=1)
 
-    gdf = gdf.drop(['x', 'y'],axis=1)
-    
-    if out_fname is not None:
+    if out_fname:
         gdf.to_file(out_fname)
-    
+
     return gdf
