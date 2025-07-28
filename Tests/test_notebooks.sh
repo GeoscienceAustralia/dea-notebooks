fb5f1ee0e1727fda650323e4eca8e0e31282bc29
robbibt <Robbi.BishopTaylor@ga.gov.au>
2025-07-28 07:24:18 +0000

    Temporarily ignore notebook

--- Tests/test_notebooks.sh
+++ Tests/test_notebooks.sh
@@ -39,7 +39,7 @@ if [ -z "$1" ]; then
         How_to_guides/Tidal_modelling.ipynb \
         How_to_guides/Using_load_ard.ipynb \
         How_to_guides/Virtual_products.ipynb \
-        How_to_guides/Generating_COG_mosaics.ipynb \
+        # How_to_guides/Generating_COG_mosaics.ipynb \  # temporarily removed until S3 access bug is fixed
         Real_world_examples/Coastal_erosion.ipynb \
         Real_world_examples/Intertidal_elevation.ipynb
 
@@ -61,7 +61,8 @@ elif [ "$1" = "how_to_guides" ]; then
         --ignore How_to_guides/Land_cover_pixel_drill.ipynb \
         --ignore How_to_guides/External_data_ERA5_Climate.ipynb \
         --ignore How_to_guides/Imagery_on_web_map.ipynb \
-        --ignore How_to_guides/Continental_scale_animations.ipynb
+        --ignore How_to_guides/Continental_scale_animations.ipynb \
+        --ignore How_to_guides/Generating_COG_mosaics.ipynb  # temporarily removed until S3 access bug is fixed
 
 elif [ "$1" = "real_world_examples" ]; then
     echo "Testing Real_world_examples..."
